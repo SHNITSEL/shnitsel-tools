3926ffb4ded6b780c3f66410ce8740ec57880280
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-01-08 09:37:20 +0100

    Implement sorting by trajid; add metadata

--- shnitsel/dynamic/parse/__init__.py
+++ shnitsel/dynamic/parse/__init__.py
@@ -14,14 +14,14 @@ _exnum = re.compile('[0-9]+')
 
 def _default_idfn(path):
     global _exnum
-    return int(os.path.basename(_exnum.search(path)[0]))
+    return int(_exnum.search(os.path.basename(path))[0])
 
 
-def read_traj_frames(path, kind, idfn=None):
+def read_traj_frames(trajdir, kind, idfn=None):
     global _default_idfn
     if idfn is None:
         idfn = _default_idfn
-    trajid = idfn(path)
+    trajid = idfn(trajdir)
 
     if kind == 'nx':
         read_traj = nx.read_traj
@@ -29,20 +29,20 @@ def read_traj_frames(path, kind, idfn=None):
         read_traj = sharc_traj.read_traj
 
     try:
-        ds = read_traj(path)
+        ds = read_traj(trajdir)
     except:
-        logging.error(f"Error for trajectory {trajid} at path {path}")
+        logging.error(f"Error for trajectory {trajid} at path {trajdir}")
         raise
 
     if not ds.attrs['completed']:
-        logging.warn(f"Trajectory {trajid} at path {path} did not complete")
+        logging.warn(f"Trajectory {trajid} at path {trajdir} did not complete")
 
     ds.attrs['trajid'] = trajid
 
     return ds
 
 
-def read_trajs_list(path, kind):
+def read_trajs_list(path, kind, sort=True):
     from tqdm import tqdm
     from tqdm.contrib.logging import logging_redirect_tqdm
 
@@ -52,17 +52,29 @@ def read_trajs_list(path, kind):
     with logging_redirect_tqdm():
         datasets = [read_traj_frames(p, kind=kind) for p in tqdm(paths)]
 
+    if sort:
+        datasets.sort(key=lambda x: x.attrs['trajid'])
+
     return datasets
 
 
 def gather_traj_metadata(datasets):
     traj_meta = np.zeros(
-        len(datasets), dtype=[('trajid', 'i4'), ('delta_t', 'f8'), ('nsteps', 'i4')]
+        len(datasets),
+        dtype=[
+            ('trajid', 'i4'),
+            ('delta_t', 'f8'),
+            ('max_ts', 'i4'),
+            ('completed', '?'),
+            ('nsteps', 'i4'),
+        ],
     )
 
     for i, ds in enumerate(datasets):
         traj_meta['trajid'][i] = ds.attrs['trajid']
         traj_meta['delta_t'][i] = ds.attrs['delta_t']
+        traj_meta['max_ts'][i] = ds.attrs['max_ts']
+        traj_meta['completed'][i] = ds.attrs['completed']
         traj_meta['nsteps'][i] = len(ds.indexes['ts'])
 
     return traj_meta
@@ -79,6 +91,8 @@ def concat_trajs(datasets):
     frames = frames.assign_coords(trajid_=traj_meta['trajid'])
     frames = frames.assign(
         delta_t=('trajid_', traj_meta['delta_t']),
+        max_ts=('trajid_', traj_meta['max_ts']),
+        completed=('trajid_', traj_meta['completed']),
         nsteps=('trajid_', traj_meta['nsteps']),
     )
     return frames
