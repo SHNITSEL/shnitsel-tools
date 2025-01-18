619df5a24a184b39ace926255fbc3b132ac1177c
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-01-18 12:09:38 +0100

    Improve handling of missing file errors

--- shnitsel/dynamic/parse/__init__.py
+++ shnitsel/dynamic/parse/__init__.py
@@ -33,16 +33,38 @@ def read_trajs_list(paths, kind, idfn=None, sort=True):
 
     datasets = []
     with logging_redirect_tqdm():
+        missing_files = {}
+        misc_errors = {}
+        incomplete = []
         for trajdir in tqdm(paths):
             trajid = idfn(trajdir)
             try:
                 ds = read_traj(trajdir)
-            except:
-                logging.error(f"Error for trajectory {trajid} at path {trajdir}")
-                raise
+            except FileNotFoundError as err:
+                # This is fairly common and will be reported at the end
+                logging.info(
+                    f"Missing file for trajectory {trajid} at path {trajdir}:\n"
+                    + str(err)
+                    + f"\nSkipping {trajid}."
+                )
+                missing = os.path.basename(err.filename)
+                missing_files[missing] = missing_files.get(missing, []) + [trajid]
+
+                continue
+            except Exception as err:
+                # Miscellaneous exceptions could indicate a problem with the parser
+                # so they enjoy a more imposing loglevel
+                logging.error(
+                    f"Error for trajectory {trajid} at path {trajdir}:\n"
+                    + str(err)
+                    + f"\nSkipping {trajid}."
+                )
+                misc_errors[trajid] = err
+                continue
 
             if not ds.attrs['completed']:
-                logging.warn(f"Trajectory {trajid} at path {trajdir} did not complete")
+                logging.info(f"Trajectory {trajid} at path {trajdir} did not complete")
+                incomplete.append(trajid)
 
             ds.attrs['trajid'] = trajid
 
@@ -50,6 +72,25 @@ def read_trajs_list(paths, kind, idfn=None, sort=True):
 
     if sort:
         datasets.sort(key=lambda x: x.attrs['trajid'])
+
+    if len(misc_errors):
+        print("Miscellaneous errors:")
+        for trajid, err in misc_errors.items():
+            print(f"{trajid:>6}  {err}")
+    if len(missing_files):
+        for fname, trajids in missing_files.items():
+            trajids.sort()
+            print(
+                f"Skipped {len(trajids)} trajectories missing file '{fname}', IDs:",
+                ' '.join([str(t) for t in trajids]),
+            )
+
+    if len(incomplete):
+        incomplete.sort()
+        print(
+            f"Included {len(incomplete)} incomplete trajectories, IDs:",
+            ' '.join([str(i) for i in incomplete]),
+        )
 
     return datasets
 
@@ -109,8 +150,8 @@ def layer_trajs(datasets):
     return layers
 
 
-def read_trajs(path, kind, format='frames'):
-    glob_expr = os.path.join(path, 'TRAJ*')
+def read_trajs(path, kind, glob_suffix='TRAJ*', format='frames'):
+    glob_expr = os.path.join(path, glob_suffix)
     paths = glob.glob(glob_expr)
     if len(paths) == 0:
         raise FileNotFoundError(
