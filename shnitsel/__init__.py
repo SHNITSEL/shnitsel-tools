901e38985da76bf0f19c05609b2d7975cf87994e
thevro <theoeverley@gmail.com>
2025-02-24 08:07:04 +0100

    Add top-level aliases for `open_frames`, `save_frames` and various plotting functions

--- shnitsel/__init__.py
+++ shnitsel/__init__.py
@@ -1,8 +1,18 @@
-from . import static, dynamic
+from . import (
+    static as static,
+    dynamic as dynamic,
+    plot as plot,
+)
 from .dynamic import parse as parse
+from .dynamic.xrhelpers import (
+    open_frames as open_frames,
+    save_frames as save_frames,
+)
 
 __all__ = [
     'static',
     'dynamic',
-    'parse'
+    'parse',
+    'open_frames',
+    'save_frames',
 ]
---
+++ shnitsel/plot/__init__.py
@@ -0,0 +1,8 @@
+from ..static.plotting import (
+    plot_energy_hist as plot_energy_hist,
+    plot_hist as plot_hist,
+    dihedral_plot as dihedral_plot,
+)
+
+from ..dynamic.datasheet import Datasheet as Datasheet
+from ..dynamic.plot.kde import biplot_kde as biplot_kde
