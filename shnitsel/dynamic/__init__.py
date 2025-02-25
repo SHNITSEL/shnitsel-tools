86599c64a9bec3b503ba8fbef74bcbe632306ce1
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-02-25 11:52:42 +0100

    Add aliases within dynamic

--- shnitsel/dynamic/__init__.py
+++ shnitsel/dynamic/__init__.py
@@ -1,4 +1,25 @@
-from . import parse, postprocess, plotting, xrhelpers, filter_unphysical, pca_biplot
+from . import (
+    parse,
+    postprocess,
+    plotting,
+    xrhelpers,
+    filter_unphysical,
+    pca_biplot,
+    plot,
+)
+
+from .xrhelpers import (
+    open_frames as open_frames,
+    save_frames as save_frames,
+)
+from .postprocess import (
+    dihedral as dihedral,
+    get_per_state as get_per_state,
+    get_inter_state as get_inter_state,
+    assign_fosc as assign_fosc,
+)
+
+from .plot.spectra3d import spectra_all_times as spectra_all_times
 
 __all__ = [
     'parse',
@@ -7,4 +28,12 @@ __all__ = [
     'xrhelpers',
     'filter_unphysical',
     'pca_biplot',
+    'plot',
+    'open_frames',
+    'save_frames',
+    'dihedral',
+    'get_per_state',
+    'get_inter_state',
+    'assign_fosc',
+    'spectra_all_times',
 ]
