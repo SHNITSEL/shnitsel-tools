b6468a7cada027f7e45f30756beb87e5eae84747
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-05-23 13:09:12 +0200

    Implement parallel parsing

--- shnitsel/dynamic/parse/__init__.py
+++ shnitsel/dynamic/parse/__init__.py
@@ -2,6 +2,9 @@ import glob
 import os
 import logging
 import re
+from dataclasses import dataclass
+from concurrent.futures import ProcessPoolExecutor
+from typing import TypeAlias, Callable
 
 import numpy as np
 import xarray as xr
@@ -13,12 +16,24 @@ from tqdm.contrib.logging import logging_redirect_tqdm
 from . import sharc_icond as sharc_icond
 from . import nx, sharc_traj
 
+Trajid: TypeAlias = int
+
 _exnum = re.compile('[0-9]+')
+
+@dataclass
+class Trajres:
+    trajid: int
+    missing_file: str | None
+    misc_error: Exception | None
+    data: xr.Dataset | None
 
 
 def _default_idfn(path):
     global _exnum
     return int(_exnum.search(os.path.basename(path))[0])
+
+_idfn = _default_idfn
+_read_traj: Callable
 
 
 def read_trajs_list(paths, kind, idfn=None, sort=True):
@@ -69,6 +84,120 @@ def read_trajs_list(paths, kind, idfn=None, sort=True):
             ds.attrs['trajid'] = trajid
 
             datasets.append(ds)
+
+    if sort:
+        datasets.sort(key=lambda x: x.attrs['trajid'])
+
+    if len(misc_errors):
+        print("Miscellaneous errors:")
+        for trajid, err in misc_errors.items():
+            print(f"{trajid:>6}  {err}")
+    if len(missing_files):
+        for fname, trajids in missing_files.items():
+            trajids.sort()
+            print(
+                f"Skipped {len(trajids)} trajectories missing file '{fname}', IDs:",
+                ' '.join([str(t) for t in trajids]),
+            )
+
+    if len(incomplete):
+        incomplete.sort()
+        print(
+            f"Included {len(incomplete)} incomplete trajectories, IDs:",
+            ' '.join([str(i) for i in incomplete]),
+        )
+
+    return datasets
+
+
+def _per_traj(trajdir):
+    trajid = _idfn(trajdir)
+    missing_file = None
+    misc_error = None
+
+    try:
+        ds = _read_traj(trajdir)
+
+    except FileNotFoundError as err:
+        # This is fairly common and will be reported at the end
+        logging.info(
+            f"Missing file for trajectory {trajid} at path {trajdir}:\n"
+            + str(err)
+            + f"\nSkipping {trajid}."
+        )
+        missing_file = os.path.basename(err.filename)
+
+        return Trajres(
+            trajid=trajid,
+            missing_file=missing_file,
+            misc_error=misc_error,
+            data=None,
+        )
+
+    except Exception as err:
+        # Miscellaneous exceptions could indicate a problem with the parser
+        # so they enjoy a more imposing loglevel
+        logging.error(
+            f"Error for trajectory {trajid} at path {trajdir}:\n"
+            + str(err)
+            + f"\nSkipping {trajid}."
+        )
+        misc_error = err
+
+        return Trajres(
+            trajid=trajid,
+            missing_file=missing_file,
+            misc_error=misc_error,
+            data=None,
+        )
+
+    if not ds.attrs['completed']:
+        logging.info(f"Trajectory {trajid} at path {trajdir} did not complete")
+
+    ds.attrs['trajid'] = trajid
+
+    return Trajres(
+        trajid=trajid, missing_file=missing_file, misc_error=misc_error, data=ds
+    )
+
+
+def read_trajs_parallel(paths, kind, idfn=None, sort=True):
+    global _idfn
+    global _read_traj
+
+    if idfn is None:
+        _idfn = _default_idfn
+    else:
+        _idfn = idfn
+
+    if kind == 'nx':
+        _read_traj = nx.read_traj
+    elif kind == 'sharc':
+        _read_traj = sharc_traj.read_traj
+
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        res = list(executor.map(_per_traj, paths))
+
+    datasets = []
+    missing_files: dict[str, list[Trajid]] = {}
+    misc_errors: dict[str, list[Trajid]] = {}
+    incomplete: list[Trajid] = []
+    for t in res:
+        if (ds := t.data) is not None:
+            datasets.append(ds)
+            if not ds.attrs['completed']:
+                incomplete.append(t.trajid)
+
+        if (mf := t.missing_file) is not None:
+            if mf not in missing_files:
+                missing_files[mf] = []
+            missing_files[mf].append(t.trajid)
+
+        if (me := t.misc_error) is not None:
+            sme = str(me)
+            if sme not in missing_files:
+                missing_files[sme] = []
+            missing_files[sme].append(t.trajid)
 
     if sort:
         datasets.sort(key=lambda x: x.attrs['trajid'])
@@ -150,7 +279,7 @@ def layer_trajs(datasets):
     return layers
 
 
-def read_trajs(path, kind, glob_suffix='TRAJ*', format='frames'):
+def read_trajs(path, kind, glob_suffix='TRAJ*', format='frames', parallel=False):
     glob_expr = os.path.join(path, glob_suffix)
     paths = glob.glob(glob_expr)
     if len(paths) == 0:
@@ -158,7 +287,10 @@ def read_trajs(path, kind, glob_suffix='TRAJ*', format='frames'):
             f"The search '{glob_expr}' didn't match any paths "
             f"in working directory '{os.getcwd()}'"
         )
-    datasets = read_trajs_list(paths, kind)
+    if parallel:
+        datasets = read_trajs_parallel(paths, kind)
+    else:
+        datasets = read_trajs_list(paths, kind)
 
     cats = {'frames': concat_trajs, 'layers': layer_trajs}
 
