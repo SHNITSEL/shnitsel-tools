d5b4725fb9f286ad3f36efe9bbd96026de1ab5f1
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-05-26 14:58:56 +0200

    Parse pyrai2md astate, forces, atXYZ, energy & time

--- shnitsel/dynamic/parse/pyrai2md.py
+++ shnitsel/dynamic/parse/pyrai2md.py
@@ -1,4 +1,6 @@
 import os
+import math
+from itertools import combinations
 from glob import glob
 from ast import literal_eval
 import xarray as xr
@@ -19,82 +21,188 @@ def parse_md_energies(path):
 
 
 def parse_log(f, nsteps):
-    ## First we read the settings.
-    #   &molecule
-    # -------------------------------------------------------
-    #   States:                     [3]
-    #   Spin:                       [0]
-    #   Interstates:                [[1, 2], [1, 3], [2, 3]]
-    #   QMMM keyfile:               None
-    #   QMMM xyzfile:               Input
-    ##  etc.
-    for line in f:
-        if line.startswith('  &molecule'):
-            # Found settings section.
-            next(f)
-            break
-    else:  # if no break occured
-        raise ValueError("No '&molecule' section found in log file.")
+    ## First, read the settings.
+    # *---------------------------------------------------*
+    # |                                                   |
+    # |          Nonadiabatic Molecular Dynamics          |
+    # |                                                   |
+    # *---------------------------------------------------*
+    #
+    #
+    # State order:         1   2   3
+    # Multiplicity:        1   1   1
+    #
+    # QMMM key:         None
+    # QMMM xyz          Input
+    # Active atoms:     45
+    # Inactive atoms:   0
+    # Link atoms:       0
+    # Highlevel atoms:  45
+    # Midlevel atoms:   0
+    # Lowlevel atoms:   0
 
-    settings = {}
-    for line in f:
-        parts = line.strip().split(':')
-        if len(parts) != 2:
-            raise ValueError(
-                "Expected exactly one colon on each line of the '&molecule block"
-            )
-        k, v = parts
-        settings[k] = v
+    while not next(f).startswith(' *---'):
+        pass
 
-    states = literal_eval(settings['States'])
-    spin = literal_eval(settings['Spin'])
-    if len(states) != 1 or spin[0] != 0:
-        raise ValueError("Only handling singlets for now.")
-    nstates = states[0]
-    interstates = literal_eval(settings['Interstates'])
-
-    ## Get the number of atoms from a separate settings block.
-    #   Active atoms:     45
-    for line in f:
-        if line.startswith('  Active atoms'):
-            natoms = int(line.strip().split()[1])
+    for _ in range(18):
+        stripline = next(f).strip()
+        if stripline.startswith('State order:'):
+            states = np.asarray(stripline.split()[2:], dtype=int)
+            nstates = len(states)
+        if stripline.startswith('Multiplicity:'):
+            multiplicity = stripline.split()[1:]
+            for m in multiplicity:
+                if m != '1':
+                    raise ValueError("Only handling singlets for now.")
+        if stripline.startswith('Active atoms:'):
+            natoms = int(stripline.split()[2])
+    del multiplicity, m
+    nscombs = math.comb(nstates, 2)
 
     # Set up numpy arrays
     astate = np.full((nsteps), -1, dtype=int)
-    forces = np.full((nsteps, natoms, 3), np.nan)
-    # time = np.full((nsteps), np.nan)
-    # nacs = np.full((nsteps, math.comb(nstates, 2), natoms, 3), np.nan)
+    forces = np.full((nsteps, nstates, natoms, 3), np.nan)
+    atXYZ = np.full((nsteps, natoms, 3), np.nan)
+    atNames = np.full((natoms), '', dtype=str)
+    got_atNames = False
+    veloc = np.full((nsteps, natoms, 3), np.nan)
+    dcmat = np.full((nsteps, nstates, 3), np.nan)
 
     for line in f:
         ## The start of a timestep
         # Iter:        1  Ekin =           0.1291084223229551 au T =   300.00 K dt =         20 CI:   3
         # Root chosen for geometry opt   2
         if line.startswith('  Iter:'):
-            ts = int(line.strip().split()[1])
+            ts = int(line.strip().split()[1]) - 1
 
-            for line in f:
+            for _ in range(10):
                 ## Get active state
                 # A surface hopping is not allowed
                 # **
                 # At state:   2
+                line = next(f)
                 if line.startswith('  At state'):
                     astate[ts] = int(line.strip().split()[2])
+                    break
                 # A surface hopping event happened
                 # **
                 # From state:   2 to state:   3 *
                 elif line.startswith('  From state'):
                     astate[ts] = int(line.strip().split()[5])
+                    break
+            else:
+                raise ValueError(f"No state info found for Iter: {ts+1}")
 
-    print(astate)
-    raise KeyboardInterrupt
+        ## Positions:
+        #   &coordinates in Angstrom
+        # -------------------------------------------------------------------------------
+        # C          0.5765950000000000     -0.8169010000000000     -0.0775610000000000
+        # C          1.7325100000000000     -0.1032670000000000      0.1707480000000000
+        # -------------------------------------------------------------------------------
+        if line.startswith('  &coordinates'):
+            assert next(f).startswith('---')
+            if got_atNames:
+                for iatom in range(natoms):
+                    atXYZ[ts, iatom] = np.asarray(
+                        next(f).strip().split()[1:], dtype=float
+                    )
+            else:
+                for iatom in range(natoms):
+                    content = next(f).strip().split()
+                    atXYZ[ts, iatom] = np.asarray(content[1:], dtype=float)
+                    atNames[iatom] = str(content[0])
+
+            assert next(f).startswith('---')
+
+        ## Velocities:
+        #   &velocities in Bohr/au
+        # -------------------------------------------------------------------------------
+        # C          0.0003442000000000      0.0001534200000000     -0.0000597200000000
+        # C         -0.0005580000000000      0.0003118300000000     -0.0000154900000000
+        # -------------------------------------------------------------------------------
+        if line.startswith('  &velocities'):
+            assert next(f).startswith('---')
+            for iatom in range(natoms):
+                veloc[ts, iatom] = np.asarray(next(f).strip().split()[1:], dtype=float)
+            assert next(f).startswith('---')
+
+        ## Forces:
+        #   &gradient state               1 in Eh/Bohr
+        # -------------------------------------------------------------------------------
+        # C         -0.0330978534152795      0.0073099255379017      0.0082666356536386
+        # C          0.0313629524413876      0.0196036465968827      0.0060952442704520
+        # -------------------------------------------------------------------------------
+        if line.startswith('  &gradient'):
+            istate = int(line.strip().split()[2]) - 1
+            assert next(f).startswith('---')
+            for iatom in range(natoms):
+                forces[ts, istate, iatom] = np.asarray(
+                    next(f).strip().split()[1:], dtype=float
+                )
+            assert next(f).startswith('---')
+
+        # ## Derivative coupling matrix:
+        # #  &derivative coupling matrix
+        # # -------------------------------------------------------------------------------
+        # #       0.0000000000000000       0.0000000000000004      -0.0000000000000001
+        # #      -0.0000000000000004       0.0000000000000000       0.0000000000000003
+        # #       0.0000000000000001      -0.0000000000000003       0.0000000000000000
+        # # -------------------------------------------------------------------------------
+        # if line.startswith('  &derivative coupling matrix'):
+        #     assert next(f).startswith('---')
+        #     for istate1 in range(nscombs):
+        #         forces[ts, istate1] = np.asarray(next(f).strip().split(), dtype=float)
+        #     assert next(f).startswith('---')
+
+    statecomb = xr.Coordinates.from_pandas_multiindex(
+        pd.MultiIndex.from_tuples(combinations(states, 2), names=['from', 'to']),
+        dim='statecomb',
+    )
+
+    coords = statecomb.merge(
+        {
+            'ts': np.arange(nsteps),
+            'state': states,
+            'state2': states,
+            'atom': np.arange(natoms),
+            'atNames': ('atom', atNames),
+            # 'statecomb': np.arange(math.comb(nstates, 2)),
+            'direction': ['x', 'y', 'z'],
+        }
+    )
+
+    return xr.Dataset(
+        {
+            # 'dip_all': (['ts', 'state', 'state2', 'direction'], dip_all),
+            # 'dip_perm': (['ts', 'state', 'direction'], dip_perm),
+            # 'dip_trans': (['ts', 'statecomb', 'direction'], dip_trans),
+            # 'sdiag': (['ts'], sdiag),
+            'astate': (['ts'], astate, {'long_name': 'active state'}),
+            'forces': (
+                ['ts', 'state', 'atom', 'direction'],
+                forces,
+                {'units': 'hartree/bohr', 'unitdim': 'Force'},
+            ),
+            # 'has_forces': (['ts'], has_forces),
+            # 'phases': (['ts', 'state'], phases),
+            # 'nacs': (
+            #     ['ts', 'statecomb', 'atom', 'direction'],
+            #     nacs,
+            #     {'long_name': "nonadiabatic couplings", 'units': "au"},
+            # ),
+            'atXYZ': (['ts', 'atom', 'direction'], atXYZ),
+        },
+        coords=coords,
+        attrs={
+            # 'max_ts': max_ts,
+            # 'real_tmax': real_tmax,
+            # 'delta_t': delta_t,
+            # 'completed': completed,
+        },
+    )
 
 
 def read_traj(traj_path):
-    # with open(os.path.join(traj_path, 'RESULTS', 'nx.log')) as f:
-    #     single_traj = parse_nx_log(f)
-
-    # nsteps = single_traj.sizes['ts']
-
     md_energies_paths = glob(os.path.join(traj_path, '*.md.energies'))
     if (n := len(md_energies_paths)) != 1:
         raise FileNotFoundError(
@@ -112,6 +220,7 @@ def read_traj(traj_path):
     nsteps = energy.sizes['time']
     with open(os.path.join(traj_path, log_paths[0])) as f:
         single_traj = parse_log(f, nsteps)
+    single_traj = single_traj.rename(ts='time').assign_coords(time=energy['time'])
     single_traj['energy'] = energy
 
     return single_traj
