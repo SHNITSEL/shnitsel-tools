739d490dbdca024d7003bff4e9247c579723584f
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-05-29 22:54:54 +0200

    Add accessor tests

--- tests/test_accessors.py
+++ tests/test_accessors.py
@@ -8,10 +8,16 @@ def test_da_accessors():
     frames = sh.open_frames('/nc/SHNITSEL_databases/dynamic/A01_ethene_dynamic.nc')
     e_step = frames.energy.sh.sudi()
     assert isinstance(e_step, xr.DataArray)
-    xyz = frames.atXYZ.isel(frame=0).sh.to_xyz()
+    xyz = frames.atXYZ.isel(frame=0).squeeze().sh.to_xyz()
     assert isinstance(xyz, str)
     dihedrals = frames.atXYZ.sh.dihedral(0, 1, 2, 3)
     assert isinstance(dihedrals, xr.DataArray)
+
+    atom_methods = {'pairwise_dists_pca', 'dihedral', 'angle', 'distance'}
+    assert atom_methods <= set(dir(frames.atXYZ.sh))
+    astate_methods = {'hop_indices', 'trajs_with_hops', 'get_hop_types'}
+    assert astate_methods <= set(dir(frames.astate.sh))
+    # astatesh = frames.astate.sh
 
 
 if __name__ == '__main__':
