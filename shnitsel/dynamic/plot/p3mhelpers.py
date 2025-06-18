a327f0b4137a5b25d29e4120061c6fc310e897a1
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-06-18 20:34:53 +0200

    Add frames3Dgrid()

--- shnitsel/dynamic/plot/p3mhelpers.py
+++ shnitsel/dynamic/plot/p3mhelpers.py
@@ -16,6 +16,29 @@ def frame3D(atXYZ_frame: str | xr.DataArray):
     return view
 
 
+def frames3Dgrid(atXYZ: xr.DataArray):
+    n = ceil(sqrt(atXYZ.sizes['frame']))
+    view = py3Dmol.view(viewergrid=(n, n), width=1000, height=800, linked=True)
+
+    for i, (label, frameXYZ) in enumerate(atXYZ.groupby('frame')):
+        data = frameXYZ.pipe(P.to_xyz)
+        viewer = (i // n, i % n)
+        view.addModel(data, viewer=viewer)
+        view.addLabel(
+            label,
+            {
+                'useScreen': True,
+                'screenOffset': {"x": 25, "y": -50},
+            },
+            viewer=viewer,
+        )
+
+    view.setStyle({'stick': {'showNonBonded': True}})
+    view.zoomTo()
+    view.show()
+    return view
+
+
 def traj3D(traj: str | xr.DataArray):
     if isinstance(traj, xr.DataArray):
         traj = P.traj_to_xyz(traj)
