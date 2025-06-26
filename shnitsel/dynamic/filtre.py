d89b0c7c57b0b6e09c0b36a9233133b9bf645d7f
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-06-26 23:27:45 +0200

    Add 3 functions to filtre.py
    - bond_type_to_symbols
    - get_bond_types
    - get_bond_lengths

--- shnitsel/dynamic/filtre.py
+++ shnitsel/dynamic/filtre.py
@@ -1,8 +1,50 @@
 import numpy as np
 import xarray as xr
 
+from .parse.common import __atnum2symbol__
+from . import postprocess as P, xrhelpers as xh
 
-def bonds(): ...
+
+def bond_type_to_symbols(e1, e2):
+    s1 = __atnum2symbol__[e1]
+    s2 = __atnum2symbol__[e2]
+    return s1 + s2
+
+
+def get_bond_types(mol, symbols=True):
+    bond_types: dict[tuple[int, int], list[tuple[int, int]]] = {}
+    for b in mol.GetBonds():
+        a1 = b.GetBeginAtom()
+        a2 = b.GetEndAtom()
+        indices = (a1.GetIdx(), a2.GetIdx())
+        elements = tuple(sorted([a1.GetAtomicNum(), a2.GetAtomicNum()]))
+        if elements not in bond_types:
+            bond_types[elements] = []
+        bond_types[elements].append(indices)
+    if symbols:
+        return {bond_type_to_symbols(*k): v for k, v in bond_types.items()}
+    return bond_types
+
+
+def get_bond_lengths(atXYZ, bond_types=None, mol=None):
+    dists = atXYZ.pipe(P.subtract_combinations, 'atom', labels=True).pipe(P.norm)
+    if bond_types is None:
+        if mol is None:
+            mol = P.default_mol(atXYZ)
+        bond_types = get_bond_types(mol, symbols=True)
+    return (
+        xr.concat(
+            [
+                dists.sel(atomcomb=bonds).pipe(
+                    xh.expand_midx, 'atomcomb', 'bond_type', bond_type
+                )
+                for bond_type, bonds in bond_types.items()
+            ],
+            dim='atomcomb',
+        )
+        .rename({'from': 'atom1', 'to': 'atom2', 'atomcomb': 'bond'})
+        .transpose('frame', ...)
+    )
 
 
 def energy_filtranda(frames: xr.Dataset) -> xr.Dataset:
