3ebc8e33219304b219e64e11408d9d3cfe27ffb2
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-07-03 09:25:41 +0200

    Expose read_trajs, read_ase at top

--- shnitsel/__init__.py
+++ shnitsel/__init__.py
@@ -4,14 +4,8 @@ from . import (
     plot as plot,
 )
 from .core import parse as parse, postprocess as postprocess, xrhelpers as xrhelpers
-from .core.xrhelpers import (
-    open_frames as open_frames,
-)
+from .core.xrhelpers import open_frames as open_frames
+from .core.parse import read_trajs as read_trajs
+from .core.ase import read_ase as read_ase
 
-__all__ = [
-    'static',
-    'core',
-    'parse',
-    'open_frames',
-    'save_frames',
-]
+__all__ = ['plot', 'parse', 'open_frames', 'read_trajs', 'read_ase']
