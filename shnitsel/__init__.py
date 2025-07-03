bf0157cc16d812536af7211635b02fa3b068ff09
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-07-03 08:38:29 +0200

    Remove redundant top-level exposures

--- shnitsel/__init__.py
+++ shnitsel/__init__.py
@@ -6,16 +6,7 @@ from . import (
 from .dynamic import parse as parse, postprocess as postprocess, xrhelpers as xrhelpers
 from .dynamic.xrhelpers import (
     open_frames as open_frames,
-    save_frames as save_frames,
 )
-from .dynamic.postprocess import (
-    dihedral as dihedral,
-    get_per_state as get_per_state,
-    get_inter_state as get_inter_state,
-    assign_fosc as assign_fosc,
-)
-
-from .dynamic.plot.spectra3d import spectra_all_times as spectra_all_times
 
 __all__ = [
     'static',
