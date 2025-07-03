bea0e544dda06d717a876628fc0c706fd3756ca4
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-07-03 23:12:30 +0200

    Use absolute imports at top-level

--- shnitsel/__init__.py
+++ shnitsel/__init__.py
@@ -1,10 +1,14 @@
-from . import (
+from shnitsel import (
     core as core,
     plot as plot,
 )
-from .core import parse as parse, postprocess as postprocess, xrhelpers as xrhelpers
-from .core.xrhelpers import open_frames as open_frames
-from .core.parse import read_trajs as read_trajs
-from .core.ase import read_ase as read_ase
+from shnitsel.core import (
+    parse as parse,
+    postprocess as postprocess,
+    xrhelpers as xrhelpers,
+)
+from shnitsel.core.xrhelpers import open_frames as open_frames
+from shnitsel.core.parse import read_trajs as read_trajs
+from shnitsel.core.ase import read_ase as read_ase
 
 __all__ = ['plot', 'parse', 'open_frames', 'read_trajs', 'read_ase']
