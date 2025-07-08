9d92e32ba383febbf0fafc68d24d87b9cca15738
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-07-08 09:57:39 +0200

    Remove `datasheet.common.figaxs()`

--- shnitsel/core/datasheet/common.py
+++ shnitsel/core/datasheet/common.py
@@ -8,29 +8,6 @@ from ..plot.pca_biplot import figax as figax
 
 symbols = dict(energy=r"$E_i$", force=r"$\mathbf{F}_i$", dip_perm=r"$\mathbf{\mu}_i$")
 
-
-# TODO: DEPRECATE
-def figaxs(
-    fig=Figure | None,
-    axs=list[Axes] | dict[str, Axes] | None,
-    mosaic=list | str | None,
-    scale_factors=tuple[float, float],
-):
-    if scale_factors is None:
-        scale_factors = (1, 1)
-    set_size = fig is None and axs is None
-    if fig is None:
-        if len(plt.get_fignums()):
-            fig = plt.gcf()
-        else:
-            fig = plt.figure(layout='constrained')
-    if axs is None:
-        axs = fig.subplot_mosaic(mosaic=mosaic)
-    if set_size:
-        fig.set_size_inches(8.27 * scale_factors[0], 11.69 * scale_factors[1])
-    return fig, axs
-
-
 def figaxs_defaults(mosaic, scale_factors=None, height_ratios=None):
     def decorator(func):
         @wraps(func)
