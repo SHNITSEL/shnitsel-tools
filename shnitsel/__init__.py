feb983cd5e604b96b0d77335bd4c01ec59488d71
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-07-09 14:19:10 +0200

    Expose filtre at top

--- shnitsel/__init__.py
+++ shnitsel/__init__.py
@@ -6,6 +6,7 @@ from shnitsel.core import (
     parse as parse,
     postprocess as postprocess,
     xrhelpers as xrhelpers,
+    filtre as filtre,
 )
 from shnitsel.core.xrhelpers import open_frames as open_frames
 from shnitsel.core.parse import read_trajs as read_trajs
