bda28cc3329e85b438dd5a184ef2b42e779584d2
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-09-11 13:26:09 +0200

    Expose `broaden_gauss` at top level
    because it takes two DataArrays
    equal in status

--- shnitsel/__init__.py
+++ shnitsel/__init__.py
@@ -9,6 +9,7 @@ from shnitsel.core import (
     filtre as filtre,
 )
 from shnitsel.core.xrhelpers import open_frames as open_frames
+from shnitsel.core.postprocess import broaden_gauss as broaden_gauss
 from shnitsel.core.parse import read_trajs as read_trajs
 from shnitsel.core.ase import read_ase as read_ase
 
