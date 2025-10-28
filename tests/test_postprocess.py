b823aff6f2c1175316af4f72155c971f304d34cc
Kevin Höllring <kevin.hoellring@fau.de>
2025-10-28 13:53:24 +0100

    Move postprocessing tests to own test class

--- tests/test_postprocess.py
+++ tests/test_postprocess.py
@@ -23,49 +23,51 @@ def traj_butene():
     return postprocess.ts_to_time(frames)
 
 
-@given(
-    xrst.variables(
-        dims=st.just({'test1': 2, 'direction': 3, 'test2': 5}),
-        dtype=st.just(float),  # type: ignore
-    ),
-)
-def test_norm(da):
-    res = norm(da)
-    if not np.isnan(da).any():
-        assert (res >= 0).all()
-    assert len(res.dims) == len(da.dims) - 1
+class TestProcessing:
+    """Class to test all functions of the shnitsel tools related to postprocessing"""
+    @given(
+        xrst.variables(
+            dims=st.just({'test1': 2, 'direction': 3, 'test2': 5}),
+            dtype=st.just(float),  # type: ignore
+        ),
+    )
+    def test_norm(da):
+        res = norm(da)
+        if not np.isnan(da).any():
+            assert (res >= 0).all()
+        assert len(res.dims) == len(da.dims) - 1
 
 
-@given(
-    xrst.variables(
-        dims=st.just({'test1': 2, 'target': 3, 'test2': 5}),
-        dtype=st.just(float),  # type: ignore
-    ),
-)
-def test_subtract_combinations(da):
-    assume((da != np.inf).all())
-    assume((da != -np.inf).all())
-    assume((~np.isnan(da)).all())  # no NaNs allowed
-    da = xr.DataArray(da)
-    res = subtract_combinations(da, 'target')
-    for c, i, j in [(0, 1, 0), (1, 2, 0), (2, 2, 1)]:
-        da_diff = da.isel(target=i) - da.isel(target=j)
-        to_check = res.isel(targetcomb=c)
-        assert_equal(da_diff, to_check)
+    @given(
+        xrst.variables(
+            dims=st.just({'test1': 2, 'target': 3, 'test2': 5}),
+            dtype=st.just(float),  # type: ignore
+        ),
+    )
+    def test_subtract_combinations(da):
+        assume((da != np.inf).all())
+        assume((da != -np.inf).all())
+        assume((~np.isnan(da)).all())  # no NaNs allowed
+        da = xr.DataArray(da)
+        res = subtract_combinations(da, 'target')
+        for c, i, j in [(0, 1, 0), (1, 2, 0), (2, 2, 1)]:
+            da_diff = da.isel(target=i) - da.isel(target=j)
+            to_check = res.isel(targetcomb=c)
+            assert_equal(da_diff, to_check)
 
-@given(
-    xrst.variables(
-        dims=st.just({'test': 2, 'target': 4}),
-        dtype=st.just(float),  # type: ignore
-    ),
-)
-def test_pca(da):
-    assume(not np.isinf(da).any())  # no +/-inf allowed
-    assume(not np.isnan(da).any())  # no NaNs allowed
-    res = pca(da, dim='target')
-    assert isinstance(res, xr.DataArray) or isinstance(res, xr.Variable)
-    assert 'PC' in res.dims
+    @given(
+        xrst.variables(
+            dims=st.just({'test': 2, 'target': 4}),
+            dtype=st.just(float),  # type: ignore
+        ),
+    )
+    def test_pca(da):
+        assume(not np.isinf(da).any())  # no +/-inf allowed
+        assume(not np.isnan(da).any())  # no NaNs allowed
+        res = pca(da, dim='target')
+        assert isinstance(res, xr.DataArray) or isinstance(res, xr.Variable)
+        assert 'PC' in res.dims
 
 
-def test_pairwise_dists_pca():
-    pass
+    def test_pairwise_dists_pca():
+        pass
