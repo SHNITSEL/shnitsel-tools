bc2bdd94c51c5520022eda10b80ea2a71f0439d3
Kevin Hoellring <kevin.hoellring@fau.de>
2025-10-29 22:58:50 +0100

    Rewrite NewtonX tests

--- tests/io/import/test_newtonx.py
+++ tests/io/import/test_newtonx.py
@@ -3,12 +3,24 @@ import os
 import xarray as xr
 
 import shnitsel as sh
+from shnitsel.io import read
+from shnitsel.io.newtonx import parse_newtonx
 
 
 class TestNewtonX:
     """Class to test functionality related to NewtonX file format"""
 
-    def test_nx(self):
+    def test_nx_direct(self):
         # parse trajectory data from Newton-X output files
-        traj_frames_chd = sh.parse.read_trajs('tutorials/test_data/newtonx/', kind='nx')
+        traj_frames_chd = parse_newtonx(
+            'tutorials/test_data/newtonx/')
 
+    def test_nx_general_wrapper_with_kind(self):
+        # parse trajectory data from Newton-X output files
+        traj_frames_chd = read(
+            'tutorials/test_data/newtonx/', kind='newtonx')
+
+    def test_nx_detected(self):
+        # parse trajectory data from Newton-X output files
+        traj_frames_chd = read(
+            'tutorials/test_data/newtonx/', kind=None)
