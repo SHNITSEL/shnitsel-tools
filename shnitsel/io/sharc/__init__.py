af9ed0f0611bf0292bbd2d355f5e603c5c8887a7
Kevin Hoellring <kevin.hoellring@fau.de>
2025-10-29 22:43:02 +0100

    Add read_traj function to exported sharc functions

--- shnitsel/io/sharc/__init__.py
+++ shnitsel/io/sharc/__init__.py
@@ -1,4 +1,5 @@
 from .parse import parse_sharc
 from .initial_conditions import dir_of_iconds, iconds_to_frames
+from .trajectory import read_traj
 
-__all__ = ['parse_sharc', 'dir_of_iconds', 'iconds_to_frames']
+__all__ = ['parse_sharc', 'read_traj', 'dir_of_iconds', 'iconds_to_frames']
