8cb76e1b619bb0e19f761cf1d5d8fa570b7e8f06
Kevin Hoellring <kevin.hoellring@fau.de>
2025-10-29 22:43:45 +0100

    Refactor general accessor test

--- tests/test_accessors.py
+++ tests/test_accessors.py
@@ -1,43 +1,42 @@
-import shnitsel as sh
+import shnitsel as st
 import shnitsel.xarray
 import xarray as xr
 import pytest
-
-
+from shnitsel.io.sharc import read_traj, dir_of_iconds
 
 
 class TestAccessors:
     """Class to test all functions of the shnitsel accessors provided to DataArray and DataSet objects"""
     @pytest.fixture
     def traj_butene(self):
-        frames = sh.parse.read_trajs('tutorials/test_data/sharc/traj_butene', kind='sharc')
+        frames = read_traj(
+            'tutorials/test_data/sharc/traj_butene', kind='sharc')
         return frames
-
 
     @pytest.fixture
     def iconds_butene(self):
-        iconds = sh.parse.sharc_icond.dirs_of_iconds(
+        iconds = dir_of_iconds(
             'tutorials/test_data/sharc/iconds_butene'
         )
         return iconds
 
     def test_da_accessors(self, traj_butene):
-        frames = sh.postprocess.ts_to_time(traj_butene)
-        e_step = frames.energy.sh.sudi()
+        frames = st.postprocess.ts_to_time(traj_butene)
+        e_step = frames.energy.st.sudi()
         assert isinstance(e_step, xr.DataArray)
-        xyz = frames.atXYZ.isel(frame=0).squeeze().sh.to_xyz()
+        xyz = frames.atXYZ.isel(frame=0).squeeze().st.to_xyz()
         assert isinstance(xyz, str)
-        dihedrals = frames.atXYZ.sh.dihedral(0, 1, 2, 3)
+        dihedrals = frames.atXYZ.st.dihedral(0, 1, 2, 3)
         assert isinstance(dihedrals, xr.DataArray)
 
         atom_methods = {'pairwise_dists_pca', 'dihedral', 'angle', 'distance'}
-        assert atom_methods <= set(dir(frames.atXYZ.sh))
+        assert atom_methods <= set(dir(frames.atXYZ.st))
         astate_methods = {'hop_indices', 'trajs_with_hops', 'get_hop_types'}
-        assert astate_methods <= set(dir(frames.astate.sh))
+        assert astate_methods <= set(dir(frames.astate.st))
         # astatesh = frames.astate.sh
 
     def test_ds_accessors(self, traj_butene, iconds_butene):
-        assert 'ts_to_time' in dir(traj_butene.sh)
-        frames = traj_butene.sh.ts_to_time()
+        assert 'ts_to_time' in dir(traj_butene.st)
+        frames = traj_butene.st.ts_to_time()
         # TODO Add more methods -- by parametrizing.
-        assert 'iconds_to_frames' in dir(iconds_butene.sh)
+        assert 'iconds_to_frames' in dir(iconds_butene.st)
