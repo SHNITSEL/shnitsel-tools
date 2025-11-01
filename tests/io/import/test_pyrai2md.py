f44578a261ab6f09295c4c4c19bc0b1964769817
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-01 04:34:08 +0100

    Extend PyrAI2md tests

--- tests/io/import/test_pyrai2md.py
+++ tests/io/import/test_pyrai2md.py
@@ -1,9 +1,24 @@
 import shnitsel as sh
 from shnitsel.io.pyrai2md import parse_pyrai2md
+from shnitsel.io import read
 
 
 class TestPyrai2mdFunctionality:
     """Class to test functionality related to the pyrai2md file format"""
 
-    def test_parse(self):
-        parse_pyrai2md('tutorials/test_data/pyrai2md')
+    def test_parse_direct(self):
+        parse_pyrai2md('tutorials/test_data/pyrai2md/traj')
+
+    def test_parse_wrapper_single(self):
+        assert read('tutorials/test_data/pyrai2md/traj',
+                    kind='pyrai2md') is not None
+
+    def test_parse_wrapper_single_guess_kind(self):
+        assert read('tutorials/test_data/pyrai2md/traj') is not None
+
+    def test_parse_wrapper_trajectory(self):
+        assert read('tutorials/test_data/pyrai2md/',
+                    kind='pyrai2md') is not None
+
+    def test_parse_wrapper_trajectory_guess_kind(self):
+        assert read('tutorials/test_data/pyrai2md/') is not None
