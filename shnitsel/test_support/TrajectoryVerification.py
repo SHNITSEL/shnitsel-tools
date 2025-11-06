cf72bed22e42801ca7b2d3d4e38bd849904debd5
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-06 01:01:44 +0100

    Add more required attributes

--- shnitsel/test_support/TrajectoryVerification.py
+++ shnitsel/test_support/TrajectoryVerification.py
@@ -9,8 +9,8 @@ from shnitsel.units.definitions import unit_dimensions
 # TODO: FIXME: Some attributes are turned into variables on merged trajectories.
 _required_shnitsel_variables = ["energy", "forces", "time"]
 _optional_shnitsel_variables = ["atXYZ", "nacs", "dip_perm", "dip_trans", "socs", "state_names",
-                                "state_types", "states", "astate", "sdiag", "state2", "statecomb", "phases", "atNames", "atNums", "e_kin", "from", "to", "atom", "state", "direction"]
-_required_shnitsel_attributes = ["input_format", "input_type", "completed", "max_ts", "delta_t", "num_singlets", "num_doublets", "num_triplets", "t_max"]
+                                "state_types", "states", "astate", "sdiag", "state2", "statecomb", "phases", "atNames", "atNums", "e_kin", "from", "to", "atom", "state", "direction", "trajid"]
+_required_shnitsel_attributes = ["input_format", "input_format_version", "input_type", "completed", "max_ts", "delta_t", "num_singlets", "num_doublets", "num_triplets", "t_max"]
 _optional_shnitsel_attributes = ["has_forces", "trajectory_input_path", "trajid", "__original_dataset", "is_multi_trajectory"]
 
 
