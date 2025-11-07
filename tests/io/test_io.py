a82ef77163a2a00cdc20f3039e3f1dcdfe221417
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-07 06:12:38 +0100

    Update shnitsel test calls

--- tests/io/test_io.py
+++ tests/io/test_io.py
@@ -2,20 +2,20 @@ import os
 
 import xarray as xr
 
-from shnitsel.io import read_shnitsel_file
+from shnitsel.io.shnitsel.format_reader import ShnitselFormatReader
 
 
 class TestGeneralIO:
     """Class to test general frame I/O functionality"""
 
     def test_open_static(self):
-        read_shnitsel_file(
+        ShnitselFormatReader().read_trajectory(
             'tutorials/test_data/shnitsel/fixtures/butene_static/data.nc')
 
     def test_open_grid(self):
-        read_shnitsel_file(
+        ShnitselFormatReader().read_trajectory(
             'tutorials/test_data/shnitsel/fixtures/butene_grid/data.nc')
 
     def test_open_dynamic(self):
-        read_shnitsel_file(
+        ShnitselFormatReader().read_trajectory(
             'tutorials/test_data/shnitsel/fixtures/butene_dynamic/data.nc')
