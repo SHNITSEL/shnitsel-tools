593244ba1d04ae047f2155b7c260e4f8d26c315e
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-07 02:47:31 +0100

    Add note to set state names in finalization function

--- shnitsel/io/shared/trajectory_finalization.py
+++ shnitsel/io/shared/trajectory_finalization.py
@@ -1,11 +1,14 @@
 import xarray as xr
 
 from shnitsel.data.TrajectoryFormat import Trajectory, wrap_trajectory
+from shnitsel.io.helpers import LoadingParameters
 from shnitsel.units.conversion import convert_all_units_to_shnitsel_defaults
 
 
-def finalize_loaded_trajectory(dataset: xr.Dataset | None) -> Trajectory | None:
+def finalize_loaded_trajectory(dataset: xr.Dataset | None, loading_parameters: LoadingParameters|None ) -> Trajectory | None:
     if dataset is not None:
+        # TODO: FIXME: use loading_parameters to configure state names
+        # TODO: FIXME: use loading_parameters to configure state names
         unset_vars = []
         for var in dataset.variables:
             if "__assigned" in dataset[var].attrs:
