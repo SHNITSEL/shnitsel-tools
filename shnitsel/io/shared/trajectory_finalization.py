e3062c718133134c1cea93e3195d4a2ca1da6da6
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-07 05:44:23 +0100

    Apply default state types and names if possible

--- shnitsel/io/shared/trajectory_finalization.py
+++ shnitsel/io/shared/trajectory_finalization.py
@@ -1,3 +1,4 @@
+import logging
 import xarray as xr
 
 from shnitsel.data.TrajectoryFormat import Trajectory, wrap_trajectory
@@ -5,7 +6,7 @@ from shnitsel.io.helpers import LoadingParameters
 from shnitsel.units.conversion import convert_all_units_to_shnitsel_defaults
 
 
-def finalize_loaded_trajectory(dataset: xr.Dataset | None, loading_parameters: LoadingParameters|None ) -> Trajectory | None:
+def finalize_loaded_trajectory(dataset: xr.Dataset | None, loading_parameters: LoadingParameters | None) -> Trajectory | None:
     if dataset is not None:
         # TODO: FIXME: use loading_parameters to configure state names
         # TODO: FIXME: use loading_parameters to configure state names
@@ -19,6 +20,31 @@ def finalize_loaded_trajectory(dataset: xr.Dataset | None, loading_parameters: L
 
         dataset.drop_vars(unset_vars)
 
+        dataset = set_state_defaults(dataset, loading_parameters)
+
         return wrap_trajectory(convert_all_units_to_shnitsel_defaults(dataset))
-    
+
     return None
+
+
+def set_state_defaults(dataset: xr.Dataset, loading_parameters: LoadingParameters | None) -> xr.Dataset:
+
+    # TODO: FIXME: apply configured names from loading_parameters
+    nsinglets = dataset.attrs["nsinglets"]
+    ndoublets = dataset.attrs["ndoublets"]
+    ntriplets = dataset.attrs["ntriplets"]
+
+    if nsinglets >= 0 and ndoublets >= 0 and ntriplets >= 0:
+        dataset.state_types[:nsinglets] = 1
+        dataset.state_names[:nsinglets] = [f"S{i}" for i in range(nsinglets)]
+        dataset.state_types[nsinglets: nsinglets + 2 * ndoublets] = 2
+        dataset.state_names[nsinglets: nsinglets + 2 * ndoublets] = [
+            f"D{i}" for i in range(2 * ndoublets)
+        ]
+        dataset.state_types[nsinglets + 2 * ndoublets:] = 3
+        dataset.state_names[nsinglets + 2 *
+                            ndoublets:] = [f"T{i}" for i in range(3 * ntriplets)]
+    else:
+        logging.error("Could not determine state multiplicities and names")
+
+    return dataset
