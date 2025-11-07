616add95fdd32984a6e95f9121eff8028644dc91
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-07 01:14:15 +0100

    Add function to potentially wrap the trajectory format. Can be used to remove the wrapper/abstraction transparently for all users.

--- shnitsel/data/TrajectoryFormat.py
+++ shnitsel/data/TrajectoryFormat.py
@@ -18,3 +18,20 @@ class Trajectory(Proxy):
     def get_original_raw(self) -> xr.Dataset:
         # TODO: Make Proxy wrapper functions return wrappers as well
         return self.attrs["__original_dataset"]
+
+
+def wrap_trajectory(ds: xr.Dataset | Trajectory) -> Trajectory:
+    """Function to wrap a dataset in a trajectory format.
+
+    Used to hide the actual type logic of the Trajectory wrapper.
+
+    Args:
+        ds (xr.Dataset | Trajectory): The dataset or trajectory to (potentially) wrap in a Trajectory instance.
+
+    Returns:
+        Trajectory: The dataset wrapped in a Trajectory object or the original Trajectory instance.
+    """
+    if isinstance(ds, Trajectory):
+        return ds
+    else:
+        return Trajectory(ds)
