5882cd889322aa1a6f80655fa0341e0bbfc6d80e
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-07 06:36:35 +0100

    Remove further unused imports

--- shnitsel/io/sharc/__init__.py
+++ shnitsel/io/sharc/__init__.py
@@ -1,5 +1,5 @@
-from .parse import parse_sharc
-from .parse_initial_conditions import dir_of_iconds, iconds_to_frames
+
+from .parse_initial_conditions import iconds_to_frames
 from .parse_trajectory import read_traj
 
-__all__ = ['parse_sharc', 'read_traj', 'dir_of_iconds', 'iconds_to_frames']
+__all__ = ['read_traj', 'iconds_to_frames']
--- shnitsel/io/sharc/parse.py
+++
@@ -1,3 +0,0 @@
-
-def parse_sharc():
-    pass
