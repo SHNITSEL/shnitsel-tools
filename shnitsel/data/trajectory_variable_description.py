e9869488807109bdb7350b8f87aab958ee31036f
Kevin Hoellring <kevin.hoellring@fau.de>
2025-11-09 04:07:15 +0100

    Add note to check unitdim/units combination before assignment.

--- shnitsel/data/trajectory_variable_description.py
+++ shnitsel/data/trajectory_variable_description.py
@@ -25,9 +25,11 @@ def set_variable_attributes(var: xr.DataArray, attrs: VariableInfoAttributes) ->
         var (xr.DataArray): The variable/DataArray to set attributes on
         attrs (VariableInfoAttributes): The standardized set of attributes to assign to the variable
     """
+
     if attrs.long_name is not None:
         var.attrs["long_name"] = attrs.long_name
 
+    # TODO: We should add a check whether the units/unitdim combination is valid.
     if attrs.unit_info is not None:
         var.attrs["unitdim"] = attrs.unit_info.unitdim
         var.attrs["units"] = attrs.unit_info.units
