5a4c64508ba25b7321c6fcc9b7ef31a129537ddb
Kevin Höllring <kevin.hoellring@fau.de>
2025-11-10 21:15:20 +0100

    move wrap_trajectory to own file due to circular dependency.
    Also split finalization for datasets and DataTrees

--- shnitsel/data/trajectory_format.py
+++ shnitsel/data/trajectory_format.py
@@ -22,38 +22,3 @@ class _Trajectory(Proxy):
     def get_original_raw(self) -> xr.Dataset:
         # TODO: Make Proxy wrapper functions return wrappers as well
         return self.attrs["__original_dataset"]
-
-
-def wrap_trajectory(ds: Trajectory | xr.DataTree) -> Trajectory | xr.DataTree:
-    """Function to wrap a dataset or tree of datasets in a trajectory format.
-
-    Used to hide the actual type logic of the Trajectory wrapper.
-
-    Args:
-        ds ( Trajectory|xr.DataTree): The dataset to wrap in a Trajectory instance. If provided a tree, all datasets in the tree will be wrapped.
-
-    Returns:
-        Trajectory|xr.DataTree: The dataset wrapped in a Trajectory object or the original Trajectory instance. Alternatively, the DataTree with each individual dataset wrapped.
-    """
-
-    if isinstance(ds, xr.DataTree):
-        return ds.map_over_datasets(func=_wrap_single_trajectory)
-    else:
-        return _wrap_single_trajectory(ds)
-
-
-def _wrap_single_trajectory(ds: Trajectory) -> Trajectory:
-    """Function to wrap a single dataset in a trajectory format.
-
-    Used to hide the actual type logic of the Trajectory wrapper.
-
-    Args:
-        ds (xr.Dataset | Trajectory): The dataset or trajectory to (potentially) wrap in a Trajectory instance.
-
-    Returns:
-        Trajectory: The dataset wrapped in a Trajectory object or the original Trajectory instance.
-    """
-
-    if "__original_dataset" not in ds.attrs:
-        ds.attrs["__original_dataset"] = ds.copy(deep=True)
-    return ds
--- shnitsel/io/shared/trajectory_finalization.py
+++ shnitsel/io/shared/trajectory_finalization.py
@@ -1,57 +1,81 @@
 import logging
 import xarray as xr
 
-from shnitsel.data.trajectory_format import Trajectory, wrap_trajectory
+from shnitsel.data.shnitsel_db_format import ShnitselDB
+from shnitsel.data.trajectory_format import Trajectory
+from shnitsel.data.trajectory_format_wrapper import wrap_trajectory
 from shnitsel.io.helpers import LoadingParameters
-from shnitsel.io.shared.variable_flagging import is_variable_assigned,mark_variable_assigned
+from shnitsel.io.shared.variable_flagging import (
+    is_variable_assigned,
+    mark_variable_assigned,
+)
 from shnitsel.units.conversion import convert_all_units_to_shnitsel_defaults
 
 
-def finalize_loaded_trajectory(dataset: xr.Dataset | None, loading_parameters: LoadingParameters | None) -> Trajectory | None:
+def finalize_loaded_trajectory(
+    dataset: xr.Dataset | ShnitselDB | None,
+    loading_parameters: LoadingParameters | None,
+) -> Trajectory | ShnitselDB | None:
+    """Function to apply some final postprocessing common to all input routines.
+
+    Args:
+        dataset (xr.Dataset | ShnitselDB | None): The dataset, ShnitselDB object or None, if reading failed. Only individual trajectories will be post-processed here.
+        loading_parameters (LoadingParameters | None): Parameters to set some defaults.
+
+    Returns:
+        Trajectory | None: _description_
+    """
     if dataset is not None:
-        # TODO: FIXME: use loading_parameters to configure state names
-        # TODO: FIXME: use loading_parameters to configure state names
-        dataset = set_state_defaults(dataset, loading_parameters)
+        if isinstance(dataset, xr.Dataset):
+            # TODO: FIXME: use loading_parameters to configure state names
+            # TODO: FIXME: use loading_parameters to configure state names
+            dataset = set_state_defaults(dataset, loading_parameters)
 
-        unset_vars = []
-        for var in dataset.variables:
-            if is_variable_assigned(dataset[var]):
-                # Remove tags
-                del dataset[var].attrs["__assigned"]
-            else:
-                unset_vars.append(var)
+            unset_vars = []
+            for var in dataset.variables:
+                if is_variable_assigned(dataset[var]):
+                    # Remove tags
+                    del dataset[var].attrs["__assigned"]
+                else:
+                    unset_vars.append(var)
 
-        logging.debug(f"Dropping unset variables: {unset_vars}")
-        dataset = dataset.drop_vars(unset_vars)
+            logging.debug(f"Dropping unset variables: {unset_vars}")
+            dataset = dataset.drop_vars(unset_vars)
 
-        return wrap_trajectory(convert_all_units_to_shnitsel_defaults(dataset))
+            return wrap_trajectory(convert_all_units_to_shnitsel_defaults(dataset))
+        elif isinstance(dataset, ShnitselDB):
+            # TODO: FIXME: Should we post-process all individual trajectories just in case?
+            return dataset
 
     return None
 
 
-def set_state_defaults(dataset: xr.Dataset, loading_parameters: LoadingParameters | None) -> xr.Dataset:
-
+def set_state_defaults(
+    dataset: xr.Dataset, loading_parameters: LoadingParameters | None
+) -> xr.Dataset:
     # TODO: FIXME: apply configured names from loading_parameters
     nsinglets = dataset.attrs["num_singlets"]
     ndoublets = dataset.attrs["num_doublets"]
     ntriplets = dataset.attrs["num_triplets"]
 
     if nsinglets >= 0 and ndoublets >= 0 and ntriplets >= 0:
-        logging.warning("We made a best-effort guess for the names and types/multiplicities of the individual states. "
-                        "Please provide a list of state types or a function ot assign the state types to have the correct values assigned.")
+        logging.warning(
+            "We made a best-effort guess for the names and types/multiplicities of the individual states. "
+            "Please provide a list of state types or a function ot assign the state types to have the correct values assigned."
+        )
         if nsinglets > 0:
             dataset.state_types[:nsinglets] = 1
-            dataset.state_names[:nsinglets] = [
-                f"S{i}" for i in range(nsinglets)]
+            dataset.state_names[:nsinglets] = [f"S{i}" for i in range(nsinglets)]
         if ndoublets > 0:
-            dataset.state_types[nsinglets: nsinglets + 2 * ndoublets] = 2
-            dataset.state_names[nsinglets: nsinglets + 2 * ndoublets] = [
+            dataset.state_types[nsinglets : nsinglets + 2 * ndoublets] = 2
+            dataset.state_names[nsinglets : nsinglets + 2 * ndoublets] = [
                 f"D{i}" for i in range(2 * ndoublets)
             ]
         if ntriplets > 0:
-            dataset.state_types[nsinglets + 2 * ndoublets:] = 3
-            dataset.state_names[nsinglets + 2 *
-                                ndoublets:] = [f"T{i}" for i in range(3 * ntriplets)]
+            dataset.state_types[nsinglets + 2 * ndoublets :] = 3
+            dataset.state_names[nsinglets + 2 * ndoublets :] = [
+                f"T{i}" for i in range(3 * ntriplets)
+            ]
     else:
         logging.error("Could not determine state multiplicities and names")
 
