4c96db255c199c9187a63d7c4074a3292d7ddc0c
Kevin Höllring <kevin.hoellring@fau.de>
2025-11-12 17:43:13 +0100

    Reduce code duplication and debug output

--- shnitsel/io/shared/trajectory_finalization.py
+++ shnitsel/io/shared/trajectory_finalization.py
@@ -4,7 +4,7 @@ import xarray as xr
 from shnitsel.data.shnitsel_db_format import ShnitselDB
 from shnitsel.data.trajectory_format import Trajectory
 from shnitsel.data.trajectory_format_wrapper import wrap_trajectory
-from shnitsel.io.helpers import LoadingParameters
+from shnitsel.io.helpers import LoadingParameters, default_state_name_assigner, default_state_type_assigner
 from shnitsel.io.shared.variable_flagging import (
     is_variable_assigned,
     mark_variable_assigned,
@@ -26,7 +26,7 @@ def finalize_loaded_trajectory(
         Trajectory | None: _description_
     """
     if dataset is not None:
-        logging.debug(f"Finalizing: {repr(dataset)}")
+        # logging.debug(f"Finalizing: {repr(dataset)}")
         if isinstance(dataset, xr.Dataset):
             # TODO: FIXME: use loading_parameters to configure state names
             dataset = set_state_defaults(dataset, loading_parameters)
@@ -62,46 +62,14 @@ def set_state_defaults(
             "Types and names of state already set for dataset in finalization."
         )
 
-        logging.debug(f"Types: {dataset.state_types}")
-        logging.debug(f"Names: {dataset.state_names}")
+        # logging.debug(f"Types: {dataset.state_types}")
+        # logging.debug(f"Names: {dataset.state_names}")
         return dataset
 
-    logging.debug("Assigning default state names and/or types.")
+    logging.debug("Assigning default state names and/or.")
 
-    if (
-        "num_singlets" not in dataset.attrs
-        or "num_doublets" not in dataset.attrs
-        or "num_triplets" not in dataset.attrs
-    ):
-        logging.debug(f"Invalid format of dataset. No spin states set: {repr(dataset)}")
-
-    nsinglets = dataset.attrs["num_singlets"]
-    ndoublets = dataset.attrs["num_doublets"]
-    ntriplets = dataset.attrs["num_triplets"]
-
-    if nsinglets >= 0 and ndoublets >= 0 and ntriplets >= 0:
-        logging.warning(
-            "We made a best-effort guess for the names and types/multiplicities of the individual states. "
-            "Please provide a list of state types or a function ot assign the state types to have the correct values assigned."
-        )
-        if nsinglets > 0:
-            dataset.state_types[:nsinglets] = 1
-            dataset.state_names[:nsinglets] = ["S" + str(i) for i in range(nsinglets)]
-        if ndoublets > 0:
-            dataset.state_types[nsinglets : nsinglets + ndoublets] = 2
-            dataset.state_names[nsinglets : nsinglets + ndoublets] = [
-                "D" + str(i) for i in range(ndoublets)
-            ]
-        if ntriplets > 0:
-            dataset.state_types[nsinglets + ndoublets :] = 3
-            dataset.state_names[nsinglets + ndoublets :] = [
-                "T" + str(i) for i in range(3 * ntriplets)
-            ]
-    else:
-        logging.error(
-            f"Could not determine state multiplicities and names (S/D/T): ({nsinglets}/{ndoublets}/{ntriplets})"
-        )
-
-    mark_variable_assigned(dataset.state_types)
-    mark_variable_assigned(dataset.state_names)
+    if not is_variable_assigned(dataset.state_types):
+        dataset = default_state_type_assigner(dataset)
+    if not is_variable_assigned(dataset.state_names):
+        dataset = default_state_name_assigner(dataset)
     return dataset
