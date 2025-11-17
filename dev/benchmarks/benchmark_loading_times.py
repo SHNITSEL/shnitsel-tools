5282ee4c944cc21d0cab9819b6befb1ee27806b1
Kevin Höllring <kevin.hoellring@fau.de>
2025-11-17 18:38:18 +0100

    Fix output of loading times benchmark

--- dev/benchmarks/benchmark_loading_times.py
+++ dev/benchmarks/benchmark_loading_times.py
@@ -75,9 +75,9 @@ def benchmark_sharc_traj():
 
 
 def benchmark_pyrai2md():
-    global num_traj_sharc_traj, num_frames_sharc_traj
+    global num_traj_pyraim2d, num_frames_pyraim2d
     # ...
-    input_paths = ["./tutorials/test_data/pyrai2md/traj/"]
+    input_paths = ["./tutorials/test_data/pyrai2md/"]
 
     for path in input_paths:
         if os.path.exists(path):
@@ -86,21 +86,36 @@ def benchmark_pyrai2md():
             )  # type: ignore
 
             for tr in traj:
-                num_traj_sharc_traj += 1
-                num_frames_sharc_traj += tr.sizes["time"]
+                num_traj_pyraim2d += 1
+                num_frames_pyraim2d += tr.sizes["time"]
         else:
             logging.warning(f"Skipping {path}")
 
 
 logging.basicConfig()
 
-logging.getLogger().setLevel(logging._nameToLevel["DEBUG".upper()])
+reps = 1000
 
-duration_newton_x = timeit.timeit(benchmark_newton_x, number=100)
-print(f"{num_traj_newton_x},{num_frames_newton_x},{duration_newton_x}")
-duration_sharc_icond = timeit.timeit(benchmark_sharc_icond, number=100)
-print(f"{num_traj_sharc_icond},{num_frames_sharc_traj},{duration_sharc_icond}")
-duration_sharc_traj = timeit.timeit(benchmark_sharc_traj, number=100)
-print(f"{num_traj_sharc_traj},{num_frames_sharc_traj},{duration_sharc_traj}")
-duration_pyrai2md = timeit.timeit(benchmark_pyrai2md, number=100)
-print(f"{num_traj_pyraim2d},{num_frames_pyraim2d},{duration_pyrai2md}")
+# logging.getLogger().setLevel(logging._nameToLevel["DEBUG".upper()])
+
+with open("read_benchmark.dat", "w") as out:
+    duration_newton_x = timeit.timeit(benchmark_newton_x, number=reps)
+    out.write(
+        f"Newtonx:\t{num_traj_newton_x},\t{num_frames_newton_x},\t{duration_newton_x}\n"
+    )
+    out.flush()
+    duration_sharc_icond = timeit.timeit(benchmark_sharc_icond, number=reps)
+    out.write(
+        f"SHARC(icond):\t{num_traj_sharc_icond},\t{num_frames_sharc_icond},\t{duration_sharc_icond}\n"
+    )
+    out.flush()
+    duration_sharc_traj = timeit.timeit(benchmark_sharc_traj, number=reps)
+    out.write(
+        f"SHARC(traj):\t{num_traj_sharc_traj},\t{num_frames_sharc_traj},\t{duration_sharc_traj}\n"
+    )
+    out.flush()
+    duration_pyrai2md = timeit.timeit(benchmark_pyrai2md, number=reps)
+    out.write(
+        f"PyrAI2md:\t{num_traj_pyraim2d},\t{num_frames_pyraim2d},\t{duration_pyrai2md}\n"
+    )
+    out.flush()
