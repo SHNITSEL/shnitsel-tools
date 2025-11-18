a4a49837840b9863af6eb482e8d0286b73ca879b
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-11-18 22:12:25 +0100

    Propagate Variable-level attrs in `tree_to_frames()`

--- shnitsel/core/tree.py
+++ shnitsel/core/tree.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 import xarray as xr
 
 
@@ -27,7 +29,9 @@ def tree_to_frames(tree, allow_inconsistent: set | None = None) -> xr.Dataset:
         A single Dataset with trajectories stacked along a dimension ``frame``;
         attributes required to be consistent across trajectories remain attributes;
         attributes permitted to vary across trajectories become coordinates;
-        other attributes are ignored and omitted.
+        other Dataset-level attributes are ignored and omitted.
+        Variable-level attributes are checked for consistency and propagated to the
+        result.
 
     Raises
     ------
@@ -36,6 +40,8 @@ def tree_to_frames(tree, allow_inconsistent: set | None = None) -> xr.Dataset:
         violate this condition, or if any of them are missing in all trajectories
         (in which case their value is consistent but invalid); this error can be suppressed
         by specifying the appropriate attribute names in the ``allow_inconsistent`` parameter.
+        Note that suppression only works for Dataset-level attributes; inconsistency
+        amongst Variable-level attributes always raises.
 
     Examples
     --------
@@ -56,16 +62,27 @@ def tree_to_frames(tree, allow_inconsistent: set | None = None) -> xr.Dataset:
         'num_doublets',
         'num_triplets',
     }
+    # vars_ensure_unique = {'long_name', 'unitdim', 'units', 'original_units'}
     if allow_inconsistent is not None:
         ensure_unique = ensure_unique.difference(allow_inconsistent)
+        # vars_ensure_unique = vars_ensure_unique.difference(allow_inconsistent)
     datasets = []
     trajids = []
     coord_names = []
     unique_values = {k: {} for k in iter(ensure_unique)}
+    var_attrs_unique_values = {}
     for node in tree.children.values():
         for k in node.attrs:
             if k not in exclude_attrs | ensure_unique:
                 coord_names.append(k)
+
+        for varname, var in chain(node.data_vars.items(), node.coords.items()):
+            if varname not in var_attrs_unique_values:
+                var_attrs_unique_values[varname] = {}
+            for k in var.attrs:
+                if k not in var_attrs_unique_values[varname]:
+                    var_attrs_unique_values[varname][k] = {}
+
     coords = {k: ('trajid_', []) for k in coord_names}
     for i, node in enumerate(tree.children.values()):
         ds = (
@@ -84,6 +101,15 @@ def tree_to_frames(tree, allow_inconsistent: set | None = None) -> xr.Dataset:
                 unique_values[k][v] = []
             unique_values[k][v].append(node.attrs['trajid'])
 
+        for var_name, var in chain(node.data_vars.items(), node.coords.items()):
+            for var_attr_name in var_attrs_unique_values[var_name]:
+                v = var.attrs.get(var_attr_name, MissingValue)
+                if v not in var_attrs_unique_values[var_name][var_attr_name]:
+                    var_attrs_unique_values[var_name][var_attr_name][v] = []
+                var_attrs_unique_values[var_name][var_attr_name][v].append(
+                    node.attrs['trajid']
+                )
+
     attrs = {}
     messages = ""
     for k, vals in unique_values.items():
@@ -101,10 +127,32 @@ def tree_to_frames(tree, allow_inconsistent: set | None = None) -> xr.Dataset:
         else:
             attrs[k] = list(vals)[0]
 
-    if messages:
-        raise InconsistentAttributeError("The following issues arose --\n" + messages)
-
     res = xr.concat(datasets, 'frame').assign_coords(
         trajid_=(per_traj_dim_name, trajids)
     )
+
+    for var_name, var_attr_data in var_attrs_unique_values.items():
+        for var_attr_name, vals in var_attr_data.items():
+            if len(vals) != 1:
+                messages += f"- There are {len(vals)} different values for {var_attr_name} in {var_name}:\n"
+                for val, ids in vals.items():
+                    messages += f"  - {k} = {val} in {len(ids)} trajectories, "
+                    if len(ids) < 20:
+                        messages += "IDs: " + " ".join([str(x) for x in ids])
+                    else:
+                        messages += "including IDs: " + " ".join(
+                            [str(x) for x in ids[:20]]
+                        )
+                    messages += "\n"
+            elif list(vals)[0] is MissingValue:
+                messages += f"- The attribute {var_attr_name} in {var_name} is missing in all trajectories."
+            else:
+                if var_name in res.coords:
+                    res.coords[var_name].attrs[var_attr_name] = list(vals)[0]
+                else:
+                    res.data_vars[var_name].attrs[var_attr_name] = list(vals)[0]
+
+    if messages:
+        raise InconsistentAttributeError("The following issues arose --\n" + messages)
+
     return res.assign_coords(coords).assign_attrs(attrs)
