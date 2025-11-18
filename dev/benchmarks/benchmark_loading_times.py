ba78c8c43d394e2ce666350b3dc13852903b123e
Kevin Höllring <kevin.hoellring@fau.de>
2025-11-18 10:38:42 +0100

    Add test for iconds with larger dataset

--- dev/benchmarks/benchmark_loading_times.py
+++ dev/benchmarks/benchmark_loading_times.py
@@ -38,7 +38,7 @@ def benchmark_newton_x():
 def benchmark_sharc_icond():
     global num_traj_sharc_icond, num_frames_sharc_icond
     # ...
-    input_paths = ["./tutorials/test_data/sharc/iconds_butene/"]
+    input_paths = ["./tutorials/test_data/sharc/iconds_butene/", "./tutorials/test_data/sharc/iconds_test/"]
 
     for path in input_paths:
         if os.path.exists(path):
@@ -94,28 +94,28 @@ def benchmark_pyrai2md():
 
 logging.basicConfig()
 
-reps = 1000
+reps = 10
 
 # logging.getLogger().setLevel(logging._nameToLevel["DEBUG".upper()])
 
 with open("read_benchmark.dat", "w") as out:
     duration_newton_x = timeit.timeit(benchmark_newton_x, number=reps)
     out.write(
-        f"Newtonx:\t{num_traj_newton_x},\t{num_frames_newton_x},\t{duration_newton_x}\n"
+        f"Newtonx:\t{num_traj_newton_x/reps},\t{num_frames_newton_x/reps},\t{duration_newton_x}\n"
     )
     out.flush()
     duration_sharc_icond = timeit.timeit(benchmark_sharc_icond, number=reps)
     out.write(
-        f"SHARC(icond):\t{num_traj_sharc_icond},\t{num_frames_sharc_icond},\t{duration_sharc_icond}\n"
+        f"SHARC(icond):\t{num_traj_sharc_icond/reps},\t{num_frames_sharc_icond/reps},\t{duration_sharc_icond}\n"
     )
     out.flush()
     duration_sharc_traj = timeit.timeit(benchmark_sharc_traj, number=reps)
     out.write(
-        f"SHARC(traj):\t{num_traj_sharc_traj},\t{num_frames_sharc_traj},\t{duration_sharc_traj}\n"
+        f"SHARC(traj):\t{num_traj_sharc_traj/reps},\t{num_frames_sharc_traj/reps},\t{duration_sharc_traj}\n"
     )
     out.flush()
     duration_pyrai2md = timeit.timeit(benchmark_pyrai2md, number=reps)
     out.write(
-        f"PyrAI2md:\t{num_traj_pyraim2d},\t{num_frames_pyraim2d},\t{duration_pyrai2md}\n"
+        f"PyrAI2md:\t{num_traj_pyraim2d/reps},\t{num_frames_pyraim2d/reps},\t{duration_pyrai2md}\n"
     )
     out.flush()
