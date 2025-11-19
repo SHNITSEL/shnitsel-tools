18751f32dd14e7f1088afc484946cdecc4823b67
Kevin Höllring <kevin.hoellring@fau.de>
2025-11-19 12:04:35 +0100

    Implement shnitsel_db helper functions for DB decorator

---
+++ shnitsel/data/shnitsel_db_helpers.py
@@ -0,0 +1,93 @@
+from typing import Any, Callable, List, TypeVar
+import xarray as xr
+
+from shnitsel.data.shnitsel_db.db_trajectory_data import TrajectoryData
+from shnitsel.data.trajectory_format import Trajectory
+from .shnitsel_db.datatree_level import (
+    _datatree_level_attribute_key,
+    DataTreeLevel_keys,
+)
+
+T = TypeVar("T", bound=xr.DataTree)
+R = TypeVar("R", bound=xr.Dataset)
+
+
+def unwrap_single_entry_in_tree(tree: T) -> Trajectory | T:
+    """Attempts to unwrap a single dataset from a tree.
+
+    If multiple or none are found, it will return the original tree
+    If a single entry was found, will return the dataset
+
+    Args:
+        root (xr.DataTree): Root of the subtree to parse
+
+    Returns:
+        xr.Dataset|List[Any]|None: Returns None if no entry was found, a list instance if multiple entries were found or a single dataset if a single entry was found in the subtree
+    """
+
+    def collect_single_data(root: xr.DataTree) -> Trajectory | List[Any] | None:
+        """Returns the single dataset in the subtree
+
+        Args:
+            root (xr.DataTree): Root of the subtree to parse
+
+        Returns:
+            xr.Dataset|List[Any]|None: Returns None if no entry was found, a list instance if multiple entries were found or a single dataset if a single entry was found in the subtree
+        """
+        res = None
+
+        if root.dataset is not None:
+            res = root.dataset
+
+        for c_k, child in root.children.items():
+            child_res = collect_single_data(child)
+            if child_res is None:
+                continue
+            elif isinstance(child_res, list):
+                return child_res
+            elif isinstance(child_res, Trajectory):
+                if res is None:
+                    res = child_res
+                else:
+                    return []
+
+        return res
+
+    res_unwrap = collect_single_data(tree)
+    if isinstance(res_unwrap, Trajectory):
+        return res_unwrap
+    else:
+        return tree
+
+
+def aggregate_xr_over_levels(tree: T, func: Callable[[T], R], level: str) -> T | None:
+    """Apply an aggregation function to every node at a level of a db structure
+
+    Args:
+        tree (T): _description_
+        func (callable): The function to apply to that subtree
+        level (str): The target level to apply the function `func` to. See `shnitsel_db.datatree_level.py` for values.
+
+    Returns:
+        T: The resulting tree after applying the transform `func` to the subtrees.
+    """
+    new_children = {}
+    drop_keys = []
+
+    if tree.attrs[_datatree_level_attribute_key] == DataTreeLevel_keys[level]:
+        tmp_aggr: R = func(tree)
+        tmp_label = f"aggregate of subtree({tree.name})"
+        new_node = TrajectoryData(tmp_aggr, tmp_label)
+        new_children[tmp_label] = new_node
+
+    for k, child in tree.children.items():
+        child_res = aggregate_xr_over_levels(child, func, level)
+        if child_res is not None:
+            new_children[k] = child_res
+        else:
+            drop_keys.append(k)
+
+    if len(new_children) > 0:
+        return tree.copy().drop_nodes(drop_keys).assign(new_children)
+    else:
+        return None
