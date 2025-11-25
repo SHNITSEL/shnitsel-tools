93cc891cd6b1195f086da2ab11cb096b6c99a95e
Kevin Höllring <kevin.hoellring@fau.de>
2025-11-25 11:18:04 +0100

    Adapt PyRAI2md tests to new test data layout

--- tests/io/import/test_pyrai2md.py
+++ tests/io/import/test_pyrai2md.py
@@ -1,5 +1,7 @@
 import os
 import shnitsel as sh
+from shnitsel.data.shnitsel_db_format import ShnitselDB
+from shnitsel.data.trajectory_format import Trajectory
 from shnitsel.io.pyrai2md import parse_pyrai2md
 from shnitsel.io import read
 from shnitsel.io.pyrai2md.format_reader import PyrAI2mdFormatReader
@@ -7,6 +9,8 @@ from shnitsel.test_support.trajectory_verification import verify_trajectory_form
 
 
 class TestPyrai2mdFunctionality:
+    input_path = "tutorials/test_data/pyrai2md/traj_I01/"
+
     """Class to test functionality related to the pyrai2md file format"""
 
     asserted_properties_in_trajectory = [
@@ -21,18 +25,19 @@ class TestPyrai2mdFunctionality:
 
     def test_read_pyrai2md_trajs_direct(self):
         # parse trajectory data directly from pyrai2md output
-        traj = PyrAI2mdFormatReader().read_trajectory(
-            "tutorials/test_data/pyrai2md/traj"
-        )
+        traj = PyrAI2mdFormatReader().read_trajectory(self.input_path + "traj1")
+        assert (
+            traj is not None
+        ), f"Failed to load PyRAI2md trajectory from {self.input_path+ "traj1"}"
 
         assert verify_trajectory_format(
             traj, self.asserted_properties_in_trajectory
-        ), f"Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
+        ), "Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
 
     def test_pyrai2md_FormatDetection_R02(self):
         # check if the directory has appropriate files
         format_info = PyrAI2mdFormatReader().check_path_for_format_info(
-            "tutorials/test_data/pyrai2md/traj"
+            self.input_path + "traj1"
         )
         assert format_info is not None, "Did not detect any format information"
         assert (
@@ -41,32 +46,56 @@ class TestPyrai2mdFunctionality:
 
     def test_parse_wrapper_single_format(self):
         # Test if read wrapper works if kind specified
-        traj = read("tutorials/test_data/pyrai2md/traj", kind="pyrai2md")
+        traj = read(self.input_path + "traj1", kind="pyrai2md")
+        assert (
+            traj is not None
+        ), f"Failed to load PyRAI2md trajectory from {self.input_path+ "traj1"}"
+        assert isinstance(
+            traj, Trajectory
+        ), f"Loaded PyRAI2md trajectory from {self.input_path+ "traj1"} has wrong format: {type(traj)}"
 
         assert verify_trajectory_format(
             traj, self.asserted_properties_in_trajectory
-        ), f"Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
+        ), "Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
 
     def test_parse_wrapper_single_guess_kind(self):
         # Test if read wrapper works if kind specified
-        traj = read("tutorials/test_data/pyrai2md/traj")
+        traj = read(self.input_path + "traj1")
+        assert (
+            traj is not None
+        ), f"Failed to load PyRAI2md trajectory from {self.input_path+ "traj1"}"
+        assert isinstance(
+            traj, Trajectory
+        ), f"Loaded PyRAI2md trajectory from {self.input_path+ "traj1"} has wrong format: {type(traj)}"
 
         assert verify_trajectory_format(
             traj, self.asserted_properties_in_trajectory
-        ), f"Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
+        ), "Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
 
-    def test_parse_wrapper_trajectory(self):
+    def test_parse_wrapper_multi_trajectory(self):
         # Test if read wrapper works if kind specified
-        traj = read("tutorials/test_data/pyrai2md/", kind="pyrai2md")
+        traj = read(self.input_path, kind="pyrai2md")
+        assert (
+            traj is not None
+        ), f"Failed to load PyRAI2md trajectory db from {self.input_path}"
+        assert isinstance(
+            traj, ShnitselDB
+        ), f"Loaded PyRAI2md trajectory from {self.input_path} has wrong format: {type(traj)}"
 
         assert verify_trajectory_format(
             traj, self.asserted_properties_in_trajectory
-        ), f"Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
+        ), "Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
 
-    def test_parse_wrapper_trajectory_guess_kind(self):
-        # Test if read wrapper works if kind specified
-        traj = read("tutorials/test_data/pyrai2md/traj")
+    def test_parse_wrapper_multi_trajectory_guess_kind(self):
+        # Test if read wrapper works if kind not specified
+        traj = read(self.input_path)
+        assert (
+            traj is not None
+        ), f"Failed to load PyRAI2md trajectory db from {self.input_path}"
+        assert isinstance(
+            traj, ShnitselDB
+        ), f"Loaded PyRAI2md trajectory from {self.input_path} has wrong format: {type(traj)}"
 
         assert verify_trajectory_format(
             traj, self.asserted_properties_in_trajectory
-        ), f"Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
+        ), "Resulting trajectory from PyrAI2md trajectory does not satisfy the Shnitsel standard format"
