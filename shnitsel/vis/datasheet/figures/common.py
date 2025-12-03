1827d33663fa697fc10aa052c289715c95b6cb4f
Kevin Hoellring <kevin.hoellring@fau.de>
2025-12-03 21:29:25 +0100

    Add documentation to datasheet figure common functions

--- shnitsel/vis/datasheet/figures/common.py
+++ shnitsel/vis/datasheet/figures/common.py
@@ -1,14 +1,31 @@
+from collections.abc import Sequence
 from functools import wraps
+from typing import Hashable
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
+from matplotlib.typing import HashableList
+from matplotlib.text import Text
 
 from ...plot.common import figax as figax
 
 symbols = dict(energy=r"$E_i$", force=r"$\mathbf{F}_i$", dip_perm=r"$\mathbf{\mu}_i$")
 
-def figaxs_defaults(mosaic, scale_factors=None, height_ratios=None):
+
+def figaxs_defaults(
+    mosaic: list[HashableList[Hashable]],
+    scale_factors: Sequence[float] | None = None,
+    height_ratios: Sequence[float] | None = None,
+):
+    """Decorator to automatically create a mosaic of subfigures and provide the axes to the decorated function if only a figure is provided.
+
+    Args:
+        mosaic (list[HashableList[Hashable]]): Matrix of keys, where the individual subplots should go
+        scale_factors (Sequence[float] , optional): Sequence of scale factors for the individual plots. Defaults to None.
+        height_ratios (Sequence[float] , optional): Height ratios of the individual plots. Defaults to None.
+    """
+
     def decorator(func):
         @wraps(func)
         def wrapper(
@@ -33,7 +50,20 @@ def figaxs_defaults(mosaic, scale_factors=None, height_ratios=None):
 
     return decorator
 
-def centertext(text, ax, clearticks='y'):
+
+def centertext(text: str, ax: Axes, clearticks='y') -> Text:
+    """Helper method to center the text within the axes.
+
+    Optionally removes ticks in the dimensions `x` or `y`.
+
+    Args:
+        text (str): Message to center in the frame
+        ax (Axes): Axes to plot the text into
+        clearticks (str, optional): String of all dimensions to clear the ticks for (may contain `x` and/or `y`). Defaults to 'y'.
+
+    Returns:
+        Text: The Text object created by a call to `.text()` on the `ax` object.
+    """
     if 'x' in clearticks:
         ax.tick_params(axis='x', labelbottom=False)
     if 'y' in clearticks:
