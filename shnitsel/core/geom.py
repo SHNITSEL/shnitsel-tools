e817f2808742e334f17712f3c9c9918d2ff00117
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-12-06 16:07:23 +0100

    Avoid unnecessary `.copy(data=...)` pattern

--- shnitsel/core/geom.py
+++ shnitsel/core/geom.py
@@ -139,24 +139,6 @@ def bond_type_to_symbols(e1, e2):
     return s1 + s2
 
 
-def identify_bonds(mol: Mol, symbols: bool = True) -> dict:
-    bond_types: dict[tuple[int, int], list[tuple[int, int]]] = {}
-    for b in mol.GetBonds():
-        a1 = b.GetBeginAtom()
-        a2 = b.GetEndAtom()
-        indices = (a1.GetIdx(), a2.GetIdx())
-        elements = tuple(sorted([a1.GetAtomicNum(), a2.GetAtomicNum()]))
-        if elements not in bond_types:
-            bond_types[elements] = []
-        bond_types[elements].append(indices)
-    if symbols:
-        return {bond_type_to_symbols(*k): v for k, v in bond_types.items()}
-    return bond_types
-
-def _fake_identify(mol):
-    raise NotImplementedError
-
-
 def _check_matches(matches_or_mol, atXYZ):
     if matches_or_mol is None:
         mol = default_mol(atXYZ)
@@ -174,10 +156,6 @@ def _check_matches(matches_or_mol, atXYZ):
     return matches
 
 
-# def _positions(atXYZ, atom_idxs):
-#     return [atXYZ.sel(atom=list(idxs)).data for idxs in zip(*atom_idxs)]
-
-
 def _positions(atXYZ, atom_idxs):
     return [
         atXYZ.sel(atom=list(idxs))
@@ -187,8 +165,8 @@ def _positions(atXYZ, atom_idxs):
     ]
 
 
-def _copy_wrap(
-    data,
+def _assign_descriptor_coords(
+    obj,
     atXYZ,
     atom_idxs: list[list[int]],
     bond_idxs: list[list[int]],
@@ -226,13 +204,8 @@ def _copy_wrap(
         }
         | {f'atom{i}': ('descriptor', list(x)) for i, x in enumerate(zip(*atom_idxs))}
     )
-    size = data.shape[atXYZ.get_axis_num('atom')]
-    template = (
-        atXYZ.isel(atom=np.zeros(size, dtype=int), direction=0)
-        .drop(['atom', 'atNames', 'direction'], errors='ignore')
-        .rename(atom='descriptor')
-    )
-    return template.copy(data=data).assign_coords(coords).set_xindex('descriptor_tex')
+
+    return obj.assign_coords(coords).set_xindex('descriptor_tex')
 
 
 @needs(dims={'atom', 'direction'})
@@ -273,55 +246,10 @@ def get_bond_lengths(
 
     r0, r1 = _positions(atXYZ, atom_idxs)
 
-    data = np.linalg.norm(r0 - r1, axis=atXYZ.get_axis_num('direction'))
+    data = dnorm(r0 - r1)
 
-    return _copy_wrap(
+    return _assign_descriptor_coords(
         data, atXYZ, atom_idxs, bond_idxs, bond_types, fragment_objs, '$r_{%d,%d}$'
-    )
-
-
-def identify_angles(mol: Mol) -> xr.Dataset:
-    triples = []
-    at_nums = []
-    bond_types = []
-    angle_types = []
-    angle_symbols = []
-    for a in mol.GetAtoms():
-        bonds = a.GetBonds()
-        if len(bonds) < 2:
-            continue
-        a0 = a.GetIdx()
-        neighbors = set()
-        for b in bonds:
-            a1, a2 = b.GetBeginAtomIdx(), b.GetEndAtomIdx()
-            neighbors |= {a1, a2}
-        neighbors -= {a0}
-        for a1, a2 in combinations(neighbors, 2):
-            triple = a1, a0, a2
-            n1, n0, n2 = (mol.GetAtomWithIdx(i).GetAtomicNum() for i in triple)
-            b10 = int(mol.GetBondBetweenAtoms(a1, a0).GetBondType())
-            b02 = int(mol.GetBondBetweenAtoms(a0, a2).GetBondType())
-            # Swap order so end-atom with lowest atomic number is first;
-            # if end-atoms have same atomic number, order by bond-type:
-            if (n1 > n2) or (n1 == n2 and b10 > b02):
-                a1, a2 = a2, a1
-                n1, n2 = n2, n1
-                b10, b02 = b02, b10
-            triples.append((a1, a0, a2))
-            at_nums.append((n1, n0, n2))
-            bond_types.append((b10, b02))
-            s = get_symbol_from_atom_number
-            angle_types.append(f"{s(n1)}{b10}{s(n0)}{b02}{s(n2)}")
-            angle_symbols.append(r"$\theta_{%d,%d,%d}$" % (a1, a0, a2))
-
-    return xr.Dataset(
-        {
-            'at_idx': (('descriptor', 'atom'), np.array(triples)),
-            'at_num': (('descriptor', 'atom'), np.array(at_nums)),
-            'bond_type': (('descriptor', 'bond'), np.array(bond_types)),
-            'descriptor_type': ('descriptor', angle_types),
-            'descriptor_tex': ('descriptor', angle_symbols),
-        }
     )
 
 
@@ -374,7 +302,7 @@ def get_bond_angles(
     if deg:
         data *= 180 / np.pi
 
-    return _copy_wrap(
+    return _assign_descriptor_coords(
         data,
         atXYZ,
         atom_idxs,
@@ -382,65 +310,6 @@ def get_bond_angles(
         bond_types,
         fragment_objs,
         r"$\theta_{%d,%d,%d}$",
-    )
-
-
-def identify_torsions(mol: Mol) -> xr.Dataset:
-    """Finds sets of four contiguous atoms that form a torsion"""
-    quadruples = []
-    at_nums = []
-    bond_types = []
-    torsion_types = []
-    torsion_symbols = []
-    for b in mol.GetBonds():
-        a1, a2 = b.GetBeginAtom(), b.GetEndAtom()
-        if a1.GetAtomicNum() > a2.GetAtomicNum():
-            a1, a2 = a2, a1
-        i1, i2 = a1.GetIdx(), a2.GetIdx()
-        bonds1, bonds2 = a1.GetBonds(), a2.GetBonds()
-        if len(bonds1) < 2 or len(bonds2) < 2:
-            continue
-        for b1, b2 in product(bonds1, bonds2):
-            atoms = [
-                b1.GetBeginAtom(),
-                b1.GetEndAtom(),
-                b2.GetBeginAtom(),
-                b2.GetEndAtom(),
-            ]
-            idxs = [a.GetIdx() for a in atoms]
-            i0 = idxs[0] if i1 == idxs[1] else idxs[1]
-            i3 = idxs[3] if i2 == idxs[2] else idxs[2]
-            if i0 == i2 or i3 == i1:
-                continue
-            idxs = [i0, i1, i2, i3]
-            atoms = [mol.GetAtomWithIdx(i) for i in idxs]
-            an = [a.GetAtomicNum() for a in atoms]
-            bt = [int(bond.GetBondType()) for bond in [b1, b, b2]]
-            if an[1] == an[2] and (
-                an[0] > an[3]  #
-                or (an[0] == an[3] and bt[0] > bt[2])  #
-            ):
-                idxs.reverse()
-                atoms.reverse()
-                an.reverse()
-                b1, b2 = b2, b1
-                bt.reverse()
-            quadruples.append(idxs)
-            at_nums.append(an)
-            bond_types.append(bt)
-            torsion_types.append(
-                '{}'.join([get_symbol_from_atom_number(n) for n in an]).format(*bt)
-            )
-            torsion_symbols.append(
-                r"$\varphi_{%d,%d,%d,%d}$" % (i0, i1, i2, i3))
-    return xr.Dataset(
-        {
-            'at_idx': (('descriptor', 'atom'), np.array(quadruples)),
-            'at_num': (('descriptor', 'atom'), np.array(at_nums)),
-            'bond_type': (('descriptor', 'bond'), np.array(bond_types)),
-            'descriptor_type': ('descriptor', torsion_types),
-            'descriptor_tex': ('descriptor', torsion_symbols),
-        }
     )
 
 
@@ -492,7 +361,7 @@ def get_bond_torsions(
     if deg:
         data *= 180 / np.pi
 
-    return _copy_wrap(
+    return _assign_descriptor_coords(
         data,
         atXYZ,
         atom_idxs,
