678be57e0cecf2a98b77295e2eabad15426ba63c
Kevin Hoellring <kevin.hoellring@fau.de>
2025-12-08 02:35:38 +0100

    Document plotting helper functions for the DataSheet

--- shnitsel/vis/plot/common.py
+++ shnitsel/vis/plot/common.py
@@ -2,13 +2,24 @@ import io
 
 import PIL
 import matplotlib as mpl
+from matplotlib.image import AxesImage
+from matplotlib.text import Annotation, Text
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure, SubFigure
 
 
-def outlabel(ax, letter):
+def outlabel(ax: Axes, label: str) -> Text:
+    """Helper function to put a label besides the graph in `ax` (outside the axes).
+
+    Args:
+        ax (Axes): The axes object to put the label next to.
+        label (str): The label text to add.
+
+    Returns:
+        Text: The created mpl.Text object.
+    """
     fixedtrans = mpl.transforms.ScaledTranslation(
         -20 / 72, +7 / 72, ax.figure.dpi_scale_trans
     )
@@ -16,7 +27,7 @@ def outlabel(ax, letter):
     return ax.text(
         0.0,
         1.0,
-        letter,
+        label,
         transform=transform,
         va='bottom',
         fontweight='bold',
@@ -24,9 +35,18 @@ def outlabel(ax, letter):
     )
 
 
-def inlabel(ax, letter):
+def inlabel(ax: Axes, label: str) -> Annotation:
+    """Helper function to add a text label inside of the axes to `ax`.
+
+    Args:
+        ax (Axes): The axes object to put the label into.
+        label (str): The label text to add.
+
+    Returns:
+        Annotation: The resulting mpl.Annotation object representing the inserted label.
+    """
     return ax.annotate(
-        letter,
+        label,
         xy=(1, 1),
         xycoords='axes fraction',
         xytext=(-1, -0.5),
@@ -43,6 +63,13 @@ def figax(
 ) -> tuple[Figure | SubFigure, Axes]:
     """
     Create figure and axes-object if an axes-object is not supplied.
+
+    Args:
+        fig (Figure | SubFigure | None, optional): The optional figure to use as a basis for `ax` if the latter is not provided. Defaults to None.
+        ax (Axes | None, optional): The axes object provided. Will be used to populate `fig` if provided.. Defaults to None.
+
+    Returns:
+        tuple[Figure | SubFigure, Axes]: A complete combination of figure and axes.
     """
     if fig is None and ax is None:
         fig, ax = plt.subplots(1, 1)
@@ -60,28 +87,39 @@ def figax(
 def extrude(x, y, xmin, xmax, ymin, ymax):
     # for extrusion, flip negative rays into quadrant 1
     if x < 0:
-        xlim = -xmin # positive
+        xlim = -xmin  # positive
         xsgn = -1
     else:
         xlim = xmax
         xsgn = 1
     if y < 0:
-        ylim = -ymin # positive
+        ylim = -ymin  # positive
         ysgn = -1
     else:
         ylim = ymax
         ysgn = 1
     # now extrude
-    x2 = abs(ylim*x/y)  # try extruding till we reach the top
-    if x2 <= xlim: # have we dropped off the right?
+    x2 = abs(ylim * x / y)  # try extruding till we reach the top
+    if x2 <= xlim:  # have we dropped off the right?
         y2 = ylim  # if not, go with this
-    else:          # but if we would have dropped off the right
+    else:  # but if we would have dropped off the right
         x2 = xlim  # just go as far right as possible instead
-        y2 = abs(xlim*y/x)
-    return x2*xsgn, y2*ysgn
+        y2 = abs(xlim * y / x)
+    return x2 * xsgn, y2 * ysgn
 
 
-def mpl_imshow_png(ax, png, **imshow_kws):
+def mpl_imshow_png(ax: Axes, png: bytes, **imshow_kws) -> AxesImage:
+    """Helper function to display an image from a bytestream input, e.g. an encoded png in axes.
+
+    Removes axes labels from `ax`.
+
+    Args:
+        ax (Axes): The axes to plot the encoded image to.
+        png (bytes): The bytestream of the encoded image.
+
+    Returns:
+        AxesImage: The image plotted to the axes `ax`.
+    """
     buffer = io.BytesIO()
     buffer.write(png)
     buffer.seek(0)
