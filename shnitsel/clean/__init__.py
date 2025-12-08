c87c60287d1f094998e3eb1c4394f9acc7f5dec5
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-12-08 12:01:11 +0100

    Add `__init__py` for `clean` and `geo`

---
+++ shnitsel/clean/__init__.py
@@ -0,0 +1,13 @@
+from .filter_energy import (
+    energy_filtranda as energy_filtranda,
+    sanity_check as sanity_check,
+)
+from .filter_geo import (
+    bond_length_filtranda as bond_length_filtranda,
+    filter_by_length as filter_by_length,
+)
+from .common import (
+    omit as omit,
+    truncate as truncate,
+    transect as transect,
+)
---
+++ shnitsel/geo/__init__.py
@@ -0,0 +1,3 @@
+import shnitsel.core.geom as calc
+
+from . import geomatch as geomatch, nonred as nonred
