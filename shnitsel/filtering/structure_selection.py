961d690c70ef254f374c694849828a236e46e4db
Kevin Hoellring <kevin.hoellring@fau.de>
2025-12-10 00:52:19 +0100

    Implement barebones structure selection wrapper class

---
+++ shnitsel/filtering/structure_selection.py
@@ -0,0 +1,91 @@
+from dataclasses import dataclass
+from typing import Self, TypeAlias
+import xarray as xr
+
+
+BondDescriptor: TypeAlias = tuple[int, int]
+AngleDescriptor: TypeAlias = tuple[int, int, int]
+DihedralDescriptor: TypeAlias = tuple[int, int, int, int]
+
+ActiveFlag: TypeAlias = bool
+
+
+@dataclass
+class StructureSelection:
+    """Class to keep track of a (sub-)selection of structural features."""
+
+    bonds: list[tuple[ActiveFlag, BondDescriptor]]
+    angles: list[tuple[ActiveFlag, AngleDescriptor]]
+    dihedrals: list[tuple[ActiveFlag, DihedralDescriptor]]
+
+    def copy_or_update(
+        self,
+        bonds: list[tuple[ActiveFlag, BondDescriptor]] | None = None,
+        angles: list[tuple[ActiveFlag, AngleDescriptor]] | None = None,
+        dihedrals: list[tuple[ActiveFlag, DihedralDescriptor]] | None = None,
+        inplace: bool = False,
+    ) -> Self:
+        """Function to create a copy with replaced member values.
+
+        Meant as a helper for the `Frozen` logic of the selection, i.e. method calls return a new instance
+        instead of updating the existing instance.
+
+        Args:
+            bonds (list[tuple[ActiveFlag, BondDescriptor]], optional): The list of new Bond flags. Defaults to None.
+            angles (list[tuple[ActiveFlag, AngleDescriptor]] | None, optional): List of angles and a flag whether they are currently part of the selection. Defaults to None.
+            dihedrals (list[tuple[ActiveFlag, DihedralDescriptor]] | None, optional): List of dihedral index tuples and a flag whether they are currently a part of the selection. Defaults to None.
+            inplace (bool, optional): Flag to allow for in-place updates instead of returning a new cop. Defaults to False.
+
+        Returns:
+            StructureSelection: The selection update with the new members set. Can either be a copy if `inplace=False` or the old instance with updated members otherwise.
+        """
+        if inplace:
+            # Update and create
+            if bonds is not None:
+                self.bonds = bonds
+            if angles is not None:
+                self.angles = angles
+            if dihedrals is not None:
+                self.dihedrals = dihedrals
+
+            return self
+        else:
+            if bonds is None:
+                bonds = self.bonds
+            if angles is None:
+                angles = self.angles
+            if dihedrals is None:
+                dihedrals = self.dihedrals
+
+            return type(self)(
+                bonds=bonds,
+                angles=angles,
+                dihedrals=dihedrals,
+            )
+
+    @classmethod
+    def init_from_dataset(cls: type[Self], dataset: xr.Dataset) -> Self:
+        """Alternative constructor that creates an initial StructureSelection object from a dataset using the entire structural information in it.
+
+        Args:
+            cls (type[StructureSelection]): The type of this StructureSelection so that we can create instances of it.
+            dataset (xr.Dataset): The dataset to extract the structure information out of. Must have at least `atXYZ` variable and a `atom` dimension. Ideally, an `atom` coordinate for feature selection is also provided.
+
+        Raises:
+            ValueError: If no structural information could be extracted from the dataset
+
+        Returns:
+            StructureSelection: A structure selection object initially covering all atoms and structural features.
+        """
+        # TODO: FIXME: Implement actual feature selection with geomatch
+
+        bonds = list()
+        angles = list()
+        dihedrals = list()
+
+        # Create an initial state selection
+        return cls(
+            bonds=bonds,
+            angles=angles,
+            dihedrals=dihedrals,
+        )
--- todo.md
+++ todo.md
@@ -1,11 +1,7 @@
 # Missing files
 
 We are currently suffering from missing input files for tests:
-- Needs to be replaced: tutorials/test_data/ase/tobias_cis_new.db
-- Needs to be replaced: tutorials/test_data/ase/old_CH2NH2.db
 - TODO: Add tests for conversion
-- Check whether NACS are actually available for different formats and what units they have: unit_dimensions.nacs: "1", # NACS should be in 1/Bohr everywhere. (NOTE: in molcas: 1/Bohr, SHARC liest 1/Bohr).
-- NACS in SHARC written to output.dat (settings as well?)
 - TODO: Mask array to only consider relevant data. Would allow us to limit atom, timestep, etc. Should probably be generated at loading time and not really stored.
 - TODO: Make it possible to merge a trajectory into an already multi-trajectory. May be necessary for some users or for us extending datasets in the future.
 - TODO: FIXME: Improve error collection for multi-trajectory parsing.
@@ -13,16 +9,16 @@ We are currently suffering from missing input files for tests:
 - Do NewtonX/PyrAI2md have charge information? SHARC 4.0 has charge info. -> Not that we know of. Probably hidden in QM interface. Add annotation tools for users to change charges on trajectories.
 - TODO: Allow setting charge in annotation script? 
 - TODO: FIXME: Do not unwrap a single trajectory being read in a multi-trajectory setup. Behavior is quite unexpected.
-- TODO: FIXME: The creation of an xarray dataset seems to be extremely slow. Check alternatives to speed it up! Also: Add a profiled test file to check, where it slows down so much. I assume it is an issue with type checking/initialization? The same way that reading SHARC trajectories was extremely slow if we didn't convert from numpy arrays. 
 - TODO: Add benchmark for parallelized loading to compare to sequential loading
-- TODO: FIXME: We need to have some NACS/SOCs output for PyRAI2md for tests
 - TODO: FIXME: Shape mismatch between DCM in PyRAI2md and default trajectory setup
 - TODO: Support reading SHARC netcdf output files
-- TODO: Rework Tree structure to use lightweight tree class and only use DataTree for load/save. Allows for better type hints and more versatile data storage
 
 - TODO: Add command line tools for annotation and inspection of trajectories/netcdf files
 - TODO: Add tutorial for further CLI tools
 - TODO: Add tox for testing shnitsel with different python versions?
+
+- TODO: Implement structure selection methods in structure selection.
+- TODO: Rework Tree structure to use lightweight tree class and only use DataTree for load/save. Allows for better type hints and more versatile data storage
 
 
 python -m cProfile ./shnitsel/cli/convert_to_shnitsel.py tutorials/test_data/sharc/iconds_butene/ -o tutorials/test_data/playground/iconds_butene.nc -c butene -est bravo -basis gulasch -log debug > tutorials/test_data/playground/iconds_butene.profile.log
