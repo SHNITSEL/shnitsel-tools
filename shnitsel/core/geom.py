19faf834ccb7d64fc3c8697da456840bb4546a1d
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2025-12-10 19:19:25 +0100

    Add `get_bla_chromophor()`

--- shnitsel/core/geom.py
+++ shnitsel/core/geom.py
@@ -17,7 +17,7 @@ from .generic import norm
 from ..bridges import default_mol
 from .._contracts import needs
 
-from shnitsel.geo.geomatch import flag_bats
+from shnitsel.geo.geomatch import flag_bats, flag_bla_chromophor
 
 AtXYZ: TypeAlias = xr.DataArray
 
@@ -138,20 +138,19 @@ def ndarray_of_tuples(da, dim):
     return res
 
 
-def _check_matches(matches_or_mol, atXYZ):
+def _check_matches(matches_or_mol, atXYZ, fn=flag_bats):
     if matches_or_mol is None:
         mol = default_mol(atXYZ)
-        matches = flag_bats(mol)[0]
+        matches = fn(mol)[0]
     elif isinstance(matches_or_mol, rc.Mol):
-        matches = flag_bats(mol)[0]
+        matches = fn(mol)[0]
     elif isinstance(matches_or_mol, dict):
         matches = matches_or_mol
     else:
         raise TypeError(f"`matches_or_mol` of wrong type: {type(matches_or_mol)}")
 
-    matches = {
-        k: [t for t in v if t[0]] for k, v in matches.items()
-    }  # remove unflagged
+    # remove unflagged:
+    matches = {k: [t for t in v if t[0]] for k, v in matches.items()}
     return matches
 
 
@@ -171,6 +170,7 @@ def _assign_descriptor_coords(
     bond_types,
     fragment_objs,
     tex_pattern,
+    per_atom_coords=True,
 ):
     smiles = []
     smarts = []
@@ -200,7 +200,11 @@ def _assign_descriptor_coords(
                 [tex_pattern % atom for atom in atom_idxs],
             ),
         }
-        | {f'atom{i}': ('descriptor', list(x)) for i, x in enumerate(zip(*atom_idxs))}
+        | (
+            {f'atom{i}': ('descriptor', list(x)) for i, x in enumerate(zip(*atom_idxs))}
+            if per_atom_coords
+            else {}
+        )
     )
 
     return obj.assign_coords(coords).set_xindex('descriptor_tex')
@@ -451,6 +455,76 @@ def get_bats(
         )
 
     return xr.concat(list(d.values()), dim='descriptor')
+
+
+@needs(dims={'atom', 'direction'})
+def get_bla_chromophor(
+    atXYZ: xr.DataArray,
+    matches_or_mol: dict | Mol | None = None,
+    mol: Mol | None = None,
+    ang: Literal[False, 'deg', 'rad'] = False,
+):
+    """Identify triples of bonded atoms (using RDKit) and calculate every bond angle for each
+    frame.
+
+    Parameters
+    ----------
+    atXYZ
+        An :py:class:`xarray.DataArray` of molecular coordinates, with dimensions
+        `frame`, `atom` and `direction`
+    matches_or_mol, optional
+        A list containing information for each internal coordinate to be calculated.
+        It may be convenient to use :py:func:`shnitsel.geo.geomatch.flag_angles`
+        to create a dictionary in the correct format, and then customize it.
+        Alternatively, you may supply an RDKit ``Mol`` object, which is passed to
+        :py:func:`shnitsel.geo.geomatch.flag_bats`.
+        If this argument is omitted altogether, an RDKit ``Mol`` object is generated
+        using :py:func:`shnitsel.bridges.default_mol` and used as above.
+
+    Returns
+    -------
+        An :py:class:`xarray.DataArray` of bond angles with dimensions `frame` and `angle`.
+
+    Raises
+    ------
+    UserWarning
+        If both `matches` and `mol` are specified.
+    """
+    matches = _check_matches(matches_or_mol, atXYZ, fn=flag_bla_chromophor)['bonds']
+
+    _, atom_idxs, bond_idxs, bond_types, fragment_objs = zip(*matches)
+
+    assert all(len(x) == 2 for x in atom_idxs)
+    assert all(len(x) == 1 for x in bond_idxs)
+    assert all(len(x) == 1 for x in bond_types)
+
+    single_idxs = []
+    double_idxs = []
+    for _, idxs, _, btype, _ in matches:
+        if btype[0] == 1:
+            single_idxs.append(idxs)
+        else:
+            double_idxs.append(idxs)
+
+    sr0, sr1 = _positions(atXYZ, single_idxs)
+    dr0, dr1 = _positions(atXYZ, double_idxs)
+
+    data = dnorm(sr0 - sr1).mean('descriptor') - dnorm(dr0 - dr1).mean('descriptor')
+    data = data.expand_dims('descriptor')
+
+    # joined_atom_idxs = tuple(reduce(lambda x, y: set(x).union(y), atom_idxs))
+    joined_atom_idxs = tuple(set(sum(bond_idxs, ())))
+    format_str = 'BLA$_{' + ','.join(['%d'] * len(joined_atom_idxs)) + '}$'
+    return _assign_descriptor_coords(
+        data,
+        # *std_args,
+        [joined_atom_idxs],
+        [sum(bond_idxs, ())],
+        [sum(bond_types, ())],
+        [rc.Mol()],
+        format_str,
+        per_atom_coords=False,
+    )
 
 
 @needs(dims={'atom', 'direction'})
