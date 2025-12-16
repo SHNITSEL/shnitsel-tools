b63311787e3e77c3a1b8f05b1f11d7814f592f9f
Kevin Hoellring <kevin.hoellring@fau.de>
2025-12-16 21:33:46 +0100

    Add function to detect whether we are running in a notebook environment

---
+++ shnitsel/core/feature_detection.py
@@ -0,0 +1,16 @@
+def is_notebook() -> bool:
+    """Helper function to detect whether we are running in a notebook environment.
+
+    Returns:
+        bool: True if we are running in a notebook ipython environment.
+    """
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
