7a592950c7e31666b388a32defeda8d228c9ba81
Kevin Höllring <kevin.hoellring@fau.de>
2025-12-17 16:30:35 +0100

    Fix return type declaration

--- shnitsel/geo/geomatch.py
+++ shnitsel/geo/geomatch.py
@@ -913,7 +913,7 @@ def flag_bats(
     mol: Mol,
     smarts: str = None,
     t_idxs: tuple = (),
-    draw=False) -> dict:
+    draw=False) -> tuple[dict,Any]:
     """
     Compute and flag bonds, angles, and dihedrals in a single call,
     automatically determining which interactions can be filtered
