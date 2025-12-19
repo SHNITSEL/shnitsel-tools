30281684c38be24a970bdd06e331da25a99be4b4
Kevin Höllring <kevin.hoellring@fau.de>
2025-12-19 14:18:58 +0100

    Disable various default imports

--- shnitsel/geo/__init__.py
+++ shnitsel/geo/__init__.py
@@ -1,6 +1,4 @@
-from . import (
-    geomatch as geomatch,
-    nonred as nonred,
-    geomatch_exact as geomatch_exact,
-    geocalc as geocalc,
-)
+# from . import (
+#     # nonred as nonred,
+#     # geocalc as geocalc,
+# )
