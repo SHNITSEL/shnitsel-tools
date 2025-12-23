287c355ce7a4f8d8c51df7c40b177ace684da4b1
Kevin Hoellring <kevin.hoellring@fau.de>
2025-12-23 05:11:35 +0100

    Add todo note to maybe clean code out if unused

--- shnitsel/core/feature_detection.py
+++ shnitsel/core/feature_detection.py
@@ -4,13 +4,14 @@ def is_notebook() -> bool:
     Returns:
         bool: True if we are running in a notebook ipython environment.
     """
+    # TODO: FIXME: Remove if unused.
     try:
         shell = get_ipython().__class__.__name__
         if shell == 'ZMQInteractiveShell':
-            return True   # Jupyter notebook or qtconsole
+            return True  # Jupyter notebook or qtconsole
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
         else:
             return False  # Other type (?)
     except NameError:
-        return False      # Probably standard Python interpreter
+        return False  # Probably standard Python interpreter
