a816aa88960297cd745865693f4828b783ca3ab5
Kevin Hoellring <kevin.hoellring@fau.de>
2025-12-29 23:12:22 +0100

    Add test stubs for clean module

---
+++ tests/clean/test_common.py
@@ -0,0 +1,1 @@
+# TODO: Test the common functions in shnitsel.clean to make sure the transect, omit, etc. all work correctly.
---
+++ tests/clean/test_sanity_check.py
@@ -0,0 +1,3 @@
+# TODO: Test the sanity check functionality for individual trajectories.
+
+# TODO: Test sanity check functionality for tree-structured datasets.
