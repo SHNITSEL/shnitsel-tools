be6c7d8088e5d78a8450060c1c339b1d625c7454
Kevin Hoellring <kevin.hoellring@fau.de>
2025-12-29 23:23:37 +0100

    Implement accessors for per-state and inter-state wrapper classes

--- shnitsel/data/dataset_containers/inter_state.py
+++ shnitsel/data/dataset_containers/inter_state.py
@@ -11,9 +11,9 @@ class InterState(ShnitselDerivedDataset):
     _original_frames: Frames
 
     def __init__(self, frames: Frames):
-        assert (
-            "state" in frames.dataset.dims
-        ), "Dataset is missing `state` dimension and cannot be considered an PerState set of variables."
+        assert "state" in frames.dataset.dims, (
+            "Dataset is missing `state` dimension and cannot be considered an PerState set of variables."
+        )
         # TODO: FIXME: Calculate per-state variables and cache in original dataset
 
         self._original_frames = frames
@@ -21,17 +21,37 @@ class InterState(ShnitselDerivedDataset):
         super().__init__(frames.dataset, per_state_props)
 
     @property
-    def delta_energy(self):
-        pass
+    def delta_energy(self) -> xr.DataArray:
+        return self.energy_interstate
 
     @property
-    def dipole_transition(self):
-        pass
+    def energy_interstate(self) -> xr.DataArray:
+        if "energy_interstate" not in self.dataset.data_vars:
+            raise KeyError(
+                "No variable `energy` to calculate interstate delta energy in trajectory data"
+            )
+        return self.dataset.data_vars["energy_interstate"]
 
     @property
-    def nacs(self):
-        pass
+    def dipole_transition(self) -> xr.DataArray:
+        if "dip_trans" not in self.dataset.data_vars:
+            raise KeyError(
+                "No variable `dip_trans` to encode interstate transition dipole moments in trajectory data"
+            )
+        return self.dataset.data_vars["dip_trans"]
 
     @property
-    def socs(self):
-        pass
+    def nacs(self) -> xr.DataArray:
+        if "nacs" not in self.dataset.data_vars:
+            raise KeyError(
+                "No variable `nacs` to encode non-adiabatic couplings in trajectory data"
+            )
+        return self.dataset.data_vars["nacs"]
+
+    @property
+    def socs(self) -> xr.DataArray:
+        if "socs" not in self.dataset.data_vars:
+            raise KeyError(
+                "No variable `socs` to encode spin-orbit-couplings in trajectory data"
+            )
+        return self.dataset.data_vars["socs"]
--- shnitsel/data/dataset_containers/per_state.py
+++ shnitsel/data/dataset_containers/per_state.py
@@ -1,19 +1,22 @@
 from dataclasses import dataclass
+from typing import Literal
 
 from shnitsel.analyze.stats import get_per_state
+from .trajectory import Trajectory
 from .shared import ShnitselDerivedDataset
 from .frames import Frames
+
 import xarray as xr
 
 
 @dataclass
 class PerState(ShnitselDerivedDataset):
-    _original_frames: Frames
+    _original_frames: Frames | Trajectory
 
-    def __init__(self, frames: Frames):
-        assert (
-            "state" in frames.dataset.dims
-        ), "Dataset is missing `state` dimension and cannot be considered an PerState set of variables."
+    def __init__(self, frames: Frames | Trajectory):
+        assert "state" in frames.dims, (
+            "Dataset is missing `state` dimension and cannot be considered an PerState set of variables."
+        )
         # TODO: FIXME: Calculate per-state variables and cache in original dataset
 
         self._original_frames = frames
@@ -21,13 +24,29 @@ class PerState(ShnitselDerivedDataset):
         super().__init__(frames.dataset, per_state_props)
 
     @property
-    def energy(self):
-        pass
+    def energy(self) -> xr.DataArray:
+        if "energy" not in self.dataset.data_vars:
+            raise KeyError(
+                "No variable `energy` to encode per-state energy in trajectory data"
+            )
+        return self.dataset.data_vars["energy"]
 
     @property
-    def dipole_permanent(self):
-        pass
+    def dipole_permanent(self) -> xr.DataArray:
+        if "dip_perm" not in self.dataset.data_vars:
+            raise KeyError(
+                "No variable `dip_perm` to encode per-state permanent dipole moments in trajectory data"
+            )
+        return self.dataset.data_vars["dip_perm"]
 
     @property
-    def forces(self):
-        pass
+    def forces(self) -> xr.DataArray:
+        if "forces" not in self.dataset.data_vars:
+            raise KeyError(
+                "No variable `forces` to encode per-state forces moments in trajectory data"
+            )
+        return self.dataset.data_vars["forces"]
+
+    @property
+    def forces_format(self) -> bool | Literal["all", "active_only"] | None:
+        return self._original_frames.forces_format
