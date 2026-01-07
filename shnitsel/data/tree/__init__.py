0900cf558117ae3c804147bb58dfbfcef2bda365
Kevin Höllring <kevin.hoellring@fau.de>
2026-01-07 15:28:46 +0100

    Add tree_merge to tree structure exports

--- shnitsel/data/tree/__init__.py
+++ shnitsel/data/tree/__init__.py
@@ -3,7 +3,7 @@ from .data_group import DataGroup, GroupInfo
 from .compound import CompoundGroup, CompoundInfo
 from .tree import ShnitselDB, ShnitselDBRoot
 
-from .support_functions import tree_zip, has_same_structure
+from .support_functions import tree_zip, has_same_structure, tree_merge
 from .node import TreeNode
 from .xr_conversion import xarray_datatree_to_shnitsel_tree, tree_to_xarray_datatree
 from .tree_completion import complete_shnitsel_tree
@@ -18,6 +18,7 @@ __all__ = [
     'GroupInfo',
     'CompoundInfo',
     'tree_zip',
+    'tree_merge',
     'has_same_structure',
     'xarray_datatree_to_shnitsel_tree',
     'tree_to_xarray_datatree',
