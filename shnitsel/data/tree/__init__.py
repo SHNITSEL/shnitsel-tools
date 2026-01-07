c4a6668171e124b0fe8817417cf28ee967968aad
Kevin Höllring <kevin.hoellring@fau.de>
2026-01-07 10:51:21 +0100

    Add exports to new tree structure

---
+++ shnitsel/data/tree/__init__.py
@@ -0,0 +1,23 @@
+from .data_leaf import DataLeaf
+from .data_group import DataGroup, GroupInfo
+from .compound import CompoundGroup, CompoundInfo
+from .tree import ShnitselDB, ShnitselDBRoot
+
+from .support_functions import tree_zip, has_same_structure
+from .node import TreeNode
+from .xr_conversion import xarray_datatree_to_shnitsel_tree, tree_to_xarray_datatree
+
+__all__ = [
+    'TreeNode',
+    'DataLeaf',
+    'DataGroup',
+    'CompoundGroup',
+    'ShnitselDBRoot',
+    'ShnitselDB',
+    'GroupInfo',
+    'CompoundInfo',
+    'tree_zip',
+    'has_same_structure',
+    'xarray_datatree_to_shnitsel_tree',
+    'tree_to_xarray_datatree',
+]
