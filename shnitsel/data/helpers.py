879fe185d8b0487ee17f92fba3e6120aaff19a00
Kevin Hoellring <kevin.hoellring@fau.de>
2026-01-08 08:43:47 +0100

    Add a stub for more sophisticated type checking

--- shnitsel/data/helpers.py
+++ shnitsel/data/helpers.py
@@ -2,6 +2,7 @@ import dataclasses
 import logging
 import math
 from typing import Dict, List, Literal, TypeVar
+from typing_extensions import TypeForm
 import xarray as xr
 import numpy as np
 
@@ -13,6 +14,9 @@ from shnitsel.core.typedefs import DatasetOrArray, Frames
 from shnitsel.units.conversion import convert_energy
 
 T = TypeVar("T")
+
+DataType = TypeVar("DataType")
+ActualDataType = TypeVar("ActualDataType")
 
 
 @internal()
@@ -46,48 +50,80 @@ def dataclass_from_dict(datatype: type[T], d: List | Dict | T) -> T:
         return d  # type: ignore # Not a dataclass field
 
 
+def is_assignable_to(
+    actual_type: type[ActualDataType] | TypeForm[ActualDataType],
+    expected_dtype: type[DataType] | TypeForm[DataType] | None,
+) -> bool:
+    """Helper function to check whether a certain type can be considered assignable to
+    another type, effectively a more general `is_subclass` function.
+
+    _extended_summary_
+
+    Parameters
+    ----------
+    actual_type : type[ActualDataType] | TypeForm[ActualDataType]
+        The exact type that an object has or that is declared as the type of an object
+    expected_dtype : type[DataType] | TypeForm[DataType] | None
+        The expected type that we would want to assign to.
+        The `actual_type` should be some combination of subclasses of the target types in `expected_dtype`.
+        If this parameter is None, the result will be True.
+
+    Returns
+    -------
+    bool
+        True if `actual_type` can be considered some form of subtype to `expected_dtype` or if the
+        expected target type is None. Otherwise False.
+
+    """
+    if expected_dtype is None:
+        return True
+    
+    # TODO: FIXME: Implement some form of type checking.
+    return True.
+
+
 # TODO: deprecate
-@needs(coords={'ts'})
+@needs(coords={"ts"})
 def ts_to_time(
     data: DatasetOrArray,
     delta_t: float | None = None,
-    old: Literal['drop', 'to_var', 'keep'] = 'drop',
+    old: Literal["drop", "to_var", "keep"] = "drop",
 ) -> DatasetOrArray:
-    assert old in {'drop', 'to_var', 'keep'}
+    assert old in {"drop", "to_var", "keep"}
 
     if delta_t is None:
-        if 'delta_t' in data:  # could be coord or var
+        if "delta_t" in data:  # could be coord or var
             # ensure unique
-            arr_delta_t = np.unique(data['delta_t'])
+            arr_delta_t = np.unique(data["delta_t"])
             assert len(arr_delta_t.shape) == 1
             if arr_delta_t.shape[0] > 1:
                 msg = "`delta_t` varies between the trajectories. Please separate the trajectories into groups"
                 raise ValueError(msg)
             delta_t = arr_delta_t.item()
-            data = data.drop_vars('delta_t')
+            data = data.drop_vars("delta_t")
 
-        if 'delta_t' in data.attrs:
+        if "delta_t" in data.attrs:
             if (
                 delta_t is not None  # If we already got delta_t from var/coord
-                and data.attrs['delta_t'] != delta_t
+                and data.attrs["delta_t"] != delta_t
             ):
                 msg = "'delta_t' attribute inconsistent with variable/coordinate"
                 raise ValueError(msg)
-            delta_t = data.attrs['delta_t']
+            delta_t = data.attrs["delta_t"]
 
         if delta_t is None:  # neither var/coord nor attr
             msg = "Could not extract `delta_t` from `data`; please pass explicitly"
             raise ValueError(msg)
 
-    data = data.reset_index('frame').assign_coords(time=data.coords['ts'] * delta_t)
-    if old in {'drop', 'to_var'}:
-        new_levels = list((set(data.indexes['frame'].names) - {'ts'}) | {'time'})
-        data = data.reset_index('frame').set_xindex(new_levels)
-    if old == 'drop':
-        data = data.drop_vars('ts')
+    data = data.reset_index("frame").assign_coords(time=data.coords["ts"] * delta_t)
+    if old in {"drop", "to_var"}:
+        new_levels = list((set(data.indexes["frame"].names) - {"ts"}) | {"time"})
+        data = data.reset_index("frame").set_xindex(new_levels)
+    if old == "drop":
+        data = data.drop_vars("ts")
 
-    data['time'].attrs.update((dict(units='fs', long_name='$t$', tex_name='t')))
-    data.attrs['delta_t'] = delta_t
+    data["time"].attrs.update((dict(units="fs", long_name="$t$", tex_name="t")))
+    data.attrs["delta_t"] = delta_t
 
     return data
 
@@ -133,7 +169,7 @@ def setup_frames(
         but cannot be carried out (e.g. because the dataset was already processed previously)
     """
     # TODO: Reconsider how the conversion works here
-    match to_time, 'time' not in ds.coords, 'ts' in ds.coords:
+    match to_time, "time" not in ds.coords, "ts" in ds.coords:
         case True, False, _:
             raise ValueError("Timestep coordinate has already been converted to time")
         case True, True, False:
@@ -141,52 +177,52 @@ def setup_frames(
         case (None, True, True) | (True, True, True):
             ds = ts_to_time(ds)
 
-    match relativize_energy, ds['energy'].min().item() != 0:
+    match relativize_energy, ds["energy"].min().item() != 0:
         case True, False:
             raise ValueError("Energy is already relativized")
         case (True, True) | (None, True):
-            assert 'energy' in ds.data_vars
+            assert "energy" in ds.data_vars
             if relativize_selector is None:
                 relativize_selector = {}
-            ds = ds.assign({'energy': relativize(ds['energy'], **relativize_selector)})
+            ds = ds.assign({"energy": relativize(ds["energy"], **relativize_selector)})
 
-    match convert_to_eV, ds['energy'].attrs.get('units') != 'eV':
+    match convert_to_eV, ds["energy"].attrs.get("units") != "eV":
         case True, False:
             raise ValueError("Energy is already in eV")
         case (True, True) | (None, True):
-            assert 'energy' in ds.data_vars
-            ds = ds.assign({'energy': convert_energy(ds['energy'], 'eV')})
+            assert "energy" in ds.data_vars
+            ds = ds.assign({"energy": convert_energy(ds["energy"], "eV")})
 
-    if convert_e_kin_to_eV and 'e_kin' not in ds.data_vars:
+    if convert_e_kin_to_eV and "e_kin" not in ds.data_vars:
         raise ValueError("'frames' object does not have an 'e_kin' variable")
-    elif 'e_kin' in ds.data_vars:
-        match convert_e_kin_to_eV, ds['e_kin'].attrs.get('units') != 'eV':
+    elif "e_kin" in ds.data_vars:
+        match convert_e_kin_to_eV, ds["e_kin"].attrs.get("units") != "eV":
             case True, False:
                 raise ValueError("Energy is already in eV")
             case (True, True) | (None, True):
-                assert 'e_kin' in ds.data_vars
-                ds = ds.assign({'e_kin': convert_energy(ds['e_kin'], 'eV')})
+                assert "e_kin" in ds.data_vars
+                ds = ds.assign({"e_kin": convert_energy(ds["e_kin"], "eV")})
 
     return ds
 
 
 def validate(frames: Frames) -> np.ndarray:
-    if 'time' in frames.coords:
-        tdim = 'time'
-    elif 'ts' in frames.coords:
-        tdim = 'ts'
+    if "time" in frames.coords:
+        tdim = "time"
+    elif "ts" in frames.coords:
+        tdim = "ts"
     else:
         raise ValueError("Found neither 'time' nor 'ts' coordinate in frames")
     bad_frames = []
     for varname in frames.data_vars.keys():
         # choose appropriate placeholder / bad value for the data_var's dtype
         dtype = frames.dtypes[varname]
-        if dtype in {np.dtype('float64'), np.dtype('float32')}:
+        if dtype in {np.dtype("float64"), np.dtype("float32")}:
             mask = np.isnan(frames[varname])
-            phname = '`nan`'
-        elif dtype in {np.dtype('int32'), np.dtype('int64')}:
+            phname = "`nan`"
+        elif dtype in {np.dtype("int32"), np.dtype("int64")}:
             mask = frames[varname] == -1
-            phname = 'placeholder `-1`'
+            phname = "placeholder `-1`"
         else:
             print(
                 f"Skipping verification of `{varname}` "
@@ -200,10 +236,10 @@ def validate(frames: Frames) -> np.ndarray:
             )
         elif mask.any():
             da = frames[varname]
-            reddims = set(da.dims) - {'frame'}
+            reddims = set(da.dims) - {"frame"}
             nans = da.sel(frame=mask.any(reddims)).frame
             n = len(nans)
-            bfstr = '; '.join(
+            bfstr = "; ".join(
                 [f"trajid={x.trajid.item()} {tdim}={x[tdim].item()}" for x in nans]
             )
             print(f"Variable `{varname}` contains {phname} in {n} frame(s),")
@@ -214,14 +250,14 @@ def validate(frames: Frames) -> np.ndarray:
 
     res: np.ndarray
     if len(bad_frames):
-        res = np.unique(xr.concat(bad_frames, dim='frame'))
+        res = np.unique(xr.concat(bad_frames, dim="frame"))
     else:
         res = np.array([])
     return res
 
 
 def split_for_saving(frames: Frames, bytes_per_chunk=50e6):
-    trajids = frames.get('trajid_', np.unique(frames['trajid']))
+    trajids = frames.get("trajid_", np.unique(frames["trajid"]))
     ntrajs = len(trajids)
     nchunks = math.trunc(frames.nbytes / 50e6)
     logging.debug(f"{nchunks=}")
