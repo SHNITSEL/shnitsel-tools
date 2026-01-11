9a982c67c79d214ee359329461ab8440228195cc
Kevin Hoellring <kevin.hoellring@fau.de>
2026-01-12 00:00:12 +0100

    Add some wrappers for the most used functions of the underlying dataset. Also add generic getattr implementation to allow for arbitrary access operations

--- shnitsel/data/dataset_containers/shared.py
+++ shnitsel/data/dataset_containers/shared.py
@@ -1,9 +1,11 @@
+from contextlib import suppress
 from dataclasses import dataclass
+from typing import Any, Hashable, Iterable, Literal, Mapping, Self
 import xarray as xr
 
 
 @dataclass
-class ShnitselDataset:
+class ShnitselDataset(object):
     _raw_dataset: xr.Dataset
 
     def __init__(self, ds: xr.Dataset):
@@ -127,6 +129,312 @@ class ShnitselDataset:
     def has(self, name: str) -> bool:
         return self.has_data(name) or self.has_dimension(name)
 
+    def sel(
+        self,
+        indexers: Mapping[Any, Any] | None = None,
+        method: str | None = None,
+        tolerance: int | float | Iterable[int | float] | None = None,
+        drop: bool = False,
+        **indexers_kwargs: Any,
+    ) -> Self:
+        """Returns a new dataset with each data array indexed by tick labels
+        along the specified dimension(s).
+
+        In contrast to `.isel`, indexers for this method should use
+        labels (i.e. explicit values in that dimension) instead of integers.
+
+        Under the hood, this method is powered by using pandas's powerful Index
+        objects. This makes label based indexing essentially just as fast as
+        using integer indexing.
+
+        It also means this method uses pandas's (well documented) logic for
+        indexing. This means you can use string shortcuts for datetime indexes
+        (e.g., '2000-01' to select all values in January 2000). It also means
+        that slices are treated as inclusive of both the start and stop values,
+        unlike normal Python indexing.
+
+        Parameters
+        ----------
+        indexers : dict, optional
+            A dict with keys matching dimensions and values given
+            by scalars, slices or arrays of tick labels. For dimensions with
+            multi-index, the indexer may also be a dict-like object with keys
+            matching index level names.
+            If DataArrays are passed as indexers, xarray-style indexing will be
+            carried out. See :ref:`indexing` for the details.
+            One of indexers or indexers_kwargs must be provided.
+        method : {None, "nearest", "pad", "ffill", "backfill", "bfill"}, optional
+            Method to use for inexact matches:
+
+            * None (default): only exact matches
+            * pad / ffill: propagate last valid index value forward
+            * backfill / bfill: propagate next valid index value backward
+            * nearest: use nearest valid index value
+        tolerance : optional
+            Maximum distance between original and new labels for inexact
+            matches. The values of the index at the matching locations must
+            satisfy the equation ``abs(index[indexer] - target) <= tolerance``.
+        drop : bool, optional
+            If ``drop=True``, drop coordinates variables in `indexers` instead
+            of making them scalar.
+        **indexers_kwargs : {dim: indexer, ...}, optional
+            The keyword arguments form of ``indexers``.
+            One of indexers or indexers_kwargs must be provided.
+
+        Returns
+        -------
+        dataset : Self
+            A new Dataset with the same contents as this dataset, except each
+            variable and dimension is indexed by the appropriate indexers.
+            If indexer DataArrays have coordinates that do not conflict with
+            this object, then these coordinates will be attached.
+            In general, each array's data will be a view of the array's data
+            in this dataset, unless vectorized indexing was triggered by using
+            an array indexer, in which case the data will be a copy.
+
+        See Also
+        --------
+        :func:`ShnitselDataset.isel <ShnitselDataset.isel>`
+        :func:`Dataset.sel <Dataset.sel>`
+        :func:`Dataset.isel <Dataset.isel>`
+        :func:`DataArray.sel <DataArray.sel>`
+
+        :doc:`xarray-tutorial:intermediate/indexing/indexing`
+            Tutorial material on indexing with Xarray objects
+
+        :doc:`xarray-tutorial:fundamentals/02.1_indexing_Basic`
+            Tutorial material on basics of indexing
+
+        """
+        # TODO: FIXME: Also select derived, base and cached variables?
+        return type(self)(
+            self._raw_dataset.sel(
+                indexers=indexers,
+                method=method,
+                tolerance=tolerance,
+                drop=drop,
+                **indexers_kwargs,
+            )
+        )
+
+    def isel(
+        self,
+        indexers: Mapping[Any, Any] | None = None,
+        drop: bool = False,
+        missing_dims: Literal["raise", "warn", "ignore"] = "raise",
+        **indexers_kwargs: Any,
+    ) -> Self:
+        """Returns a new dataset with each array indexed along the specified
+        dimension(s).
+
+        This method selects values from each array using its `__getitem__`
+        method, except this method does not require knowing the order of
+        each array's dimensions.
+
+        Parameters
+        ----------
+        indexers : dict, optional
+            A dict with keys matching dimensions and values given
+            by integers, slice objects or arrays.
+            indexer can be a integer, slice, array-like or DataArray.
+            If DataArrays are passed as indexers, xarray-style indexing will be
+            carried out. See :ref:`indexing` for the details.
+            One of indexers or indexers_kwargs must be provided.
+        drop : bool, default: False
+            If ``drop=True``, drop coordinates variables indexed by integers
+            instead of making them scalar.
+        missing_dims : {"raise", "warn", "ignore"}, default: "raise"
+            What to do if dimensions that should be selected from are not present in the
+            Dataset:
+            - "raise": raise an exception
+            - "warn": raise a warning, and ignore the missing dimensions
+            - "ignore": ignore the missing dimensions
+
+        **indexers_kwargs : {dim: indexer, ...}, optional
+            The keyword arguments form of ``indexers``.
+            One of indexers or indexers_kwargs must be provided.
+
+        Returns
+        -------
+        obj : Dataset
+            A new Dataset with the same contents as this dataset, except each
+            array and dimension is indexed by the appropriate indexers.
+            If indexer DataArrays have coordinates that do not conflict with
+            this object, then these coordinates will be attached.
+            In general, each array's data will be a view of the array's data
+            in this dataset, unless vectorized indexing was triggered by using
+            an array indexer, in which case the data will be a copy.
+
+        Examples
+        --------
+
+        # A specific element from the dataset is selected
+
+        >>> dataset.isel(atom=1, time=0)
+        <xarray.Dataset> Size:
+        Dimensions:         (direction: 3)
+        Coordinates:
+            atom        int16 2B 1
+            time        float64 8B 0.0
+            direction   (direction) <U1 3B 'x' 'y' 'z'
+        Data variables:
+            energy  float64 8B -238.2
+            forces  (direction) float64 24B 1.2 -0.2 0.1
+
+        # Indexing with a slice using isel
+
+        >>> slice_of_data = dataset.isel(atom=slice(0, 2), time=slice(0, 2))
+        >>> slice_of_data
+        <xarray.Dataset> Size:
+        Dimensions:         (atom: 2, time: 2, direction: 3)
+        Coordinates:
+            * atom         (atom) int16 2B 1
+            * time         (time) float64 16B 0.0 0.5
+            * direction    <U1 3B 'x' 'y' 'z'
+        Data variables:
+            energy      (time) float64 24B -238.2
+            forces      (time, atom, direction) float64 96B -0.5 -0.4 0.4 ...
+
+        >>> index_array = xr.DataArray([0, 2], dims="atom")
+        >>> indexed_data = dataset.isel(atom=index_array)
+        >>> indexed_data
+        <xarray.Dataset> Size:
+        Dimensions:         (atom: 2, time: 3, direction: 3)
+        Coordinates:
+          * atom            (atom) int16 4B 1 3
+          * time            (time) float64 16B 0.0 0.5 1.0
+          * direction       <U1 3B 'x' 'y' 'z'
+        Data variables:
+            energy      (time) float64 24B -238.2 -238.4 -237.9
+            forces      (time, atom, direction) float64 96B -0.5 -0.4 0.4 ...
+
+        See Also
+        --------
+        :func:`ShnitselDataset.sel <Dataset.sel>`
+        :func:`Dataset.sel <Dataset.sel>`
+        :func:`Dataset.isel <Dataset.sel>`
+        :func:`DataArray.isel <DataArray.isel>`
+
+        :doc:`xarray-tutorial:intermediate/indexing/indexing`
+            Tutorial material on indexing with Xarray objects
+
+        :doc:`xarray-tutorial:fundamentals/02.1_indexing_Basic`
+            Tutorial material on basics of indexing
+
+        """
+        # TODO: FIXME: Also select derived, base and cached variables?
+        return type(self)(
+            self._raw_dataset.isel(
+                indexers=indexers,
+                drop=drop,
+                missing_dims=missing_dims,
+                **indexers_kwargs,
+            )
+        )
+
+    def groupby(self) -> Any:
+        self._raw_dataset.groupby()
+    # @property
+    # def _attr_sources(self) -> Iterable[Mapping[Hashable, Any]]:
+    #     """Places to look-up items for attribute-style access"""
+    #     yield from ()
+
+    # @property
+    # def _item_sources(self) -> Iterable[Mapping[Hashable, Any]]:
+    #     """Places to look-up items for key-autocompletion"""
+    #     yield from ()
+
+    @property
+    def _attr_sources(self) -> Iterable[Mapping[Hashable, Any]]:
+        """Places to look-up items for attribute-style access"""
+        yield from self._item_sources
+        yield self._raw_dataset.attrs
+
+    @property
+    def _item_sources(self) -> Iterable[Mapping[Hashable, Any]]:
+        """Places to look-up items for key-completion"""
+        # print(hasattr(self, '_raw_dataset'))
+        yield from self._raw_dataset._item_sources
+
+    def __getattr__(self, name: str) -> Any:
+        if name.startswith("__"):
+            raise AttributeError(name)
+        # print(name)
+        if name not in {"__dict__", "__setstate__", "_raw_dataset"}:
+            # this avoids an infinite loop when pickle looks for the
+            # __setstate__ attribute before the object is initialized
+            for source in self._attr_sources:
+                with suppress(KeyError):
+                    return source[name]
+        raise AttributeError(
+            f"{type(self).__name__!r} object has no attribute {name!r}"
+        )
+
+    def _repr_html_(self) -> Any:
+        return self._raw_dataset._repr_html_()
+
+    # This complicated two-method design boosts overall performance of simple operations
+    # - particularly DataArray methods that perform a _to_temp_dataset() round-trip - by
+    # a whopping 8% compared to a single method that checks hasattr(self, "__dict__") at
+    # runtime before every single assignment. All of this is just temporary until the
+    # FutureWarning can be changed into a hard crash.
+    # def _setattr_dict(self, name: str, value: Any) -> None:
+    #     """Deprecated third party subclass (see ``__init_subclass__`` above)"""
+    #     object.__setattr__(self, name, value)
+    #     if name in self.__dict__:
+    #         # Custom, non-slotted attr, or improperly assigned variable?
+    #         warnings.warn(
+    #             f"Setting attribute {name!r} on a {type(self).__name__!r} object. Explicitly define __slots__ "
+    #             "to suppress this warning for legitimate custom attributes and "
+    #             "raise an error when attempting variables assignments.",
+    #             FutureWarning,
+    #             stacklevel=2,
+    #         )
+
+    # def __setattr__(self, name: str, value: Any) -> None:
+    #     # """Objects with ``__slots__`` raise AttributeError if you try setting an
+    #     # undeclared attribute. This is desirable, but the error message could use some
+    #     # improvement.
+    #     # """
+    #     # try:
+    #     #     object.__setattr__(self, name, value)
+    #     # except AttributeError as e:
+    #     #     # Don't accidentally shadow custom AttributeErrors, e.g.
+    #     #     # DataArray.dims.setter
+    #     #     if str(e) != f"{type(self).__name__!r} object has no attribute {name!r}":
+    #     #         raise
+    #     #     raise AttributeError(
+    #     #         f"cannot set attribute {name!r} on a {type(self).__name__!r} object. Use __setitem__ style"
+    #     #         "assignment (e.g., `ds['name'] = ...`) instead of assigning variables."
+    #     #     ) from e
+    #     # No support for arbitrary assignments currently
+    #     pass
+
+    def __dir__(self) -> list[str]:
+        """Provide method name lookup and completion. Only provide 'public'
+        methods.
+        """
+        extra_attrs = {
+            item
+            for source in self._attr_sources
+            for item in source
+            if isinstance(item, str)
+        }
+        return sorted(set(dir(type(self))) | extra_attrs)
+
+    def _ipython_key_completions_(self) -> list[str]:
+        """Provide method for the key-autocompletions in IPython.
+        See https://ipython.readthedocs.io/en/stable/config/integrating.html#tab-completion
+        For the details.
+        """
+        items = {
+            item
+            for source in self._item_sources
+            for item in source
+            if isinstance(item, str)
+        }
+        return list(items)
+
 
 @dataclass
 class ShnitselDerivedDataset(ShnitselDataset):
@@ -139,5 +447,13 @@ class ShnitselDerivedDataset(ShnitselDataset):
     @property
     def base(self) -> xr.Dataset | None:
         return self.base
+
+    @property
+    def _item_sources(self) -> Iterable[Mapping[Hashable, Any]]:
+        """Places to look-up items for key-completion"""
+        # print(hasattr(self, '_raw_dataset'))
+        yield from self._raw_dataset._item_sources
+        if self._base_dataset is not None:
+            yield from self._base_dataset._item_sources
 
     # TODO: Forward all unmet requests to dataset.
