43e03655067a85470545da41b7c05541db93ef0d
Kevin Hoellring <kevin.hoellring@fau.de>
2026-01-19 00:27:45 +0100

    Update tree concatenation tests

--- tests/test_tree_to_frames.py
+++ tests/test_tree_to_frames.py
@@ -6,7 +6,7 @@ from shnitsel.io import read
 
 @fixture(
     params=[
-        'tutorials/tut_data/traj_I02.nc',
+        'tutorials/test_data/shnitsel/traj_I02.nc',
         'tutorials/test_data/sharc/traj_I01_v3.0_triplets_nacs_socs',
         'tutorials/test_data/newtonx/test_I01_v2.6',
     ]
@@ -18,7 +18,11 @@ def tree(request):
 
 def test_tree_to_stacked(tree):
     frames = tree.as_stacked
+    assert ('atrajectory' in frames.dims)
+    assert ('trajectory' in frames.dims)
 
 
 def test_tree_to_layered(tree):
     layered = tree.as_layered
+    assert ('atrajectory' not in layered.dims)
+    assert ('trajectory' in layered.dims)
