4deffe96185a42f840e5b9197d8234aa7a13c003
Kevin Hoellring <kevin.hoellring@fau.de>
2026-01-19 00:39:14 +0100

    Fix tree concatenation tests

--- tests/test_tree_to_frames.py
+++ tests/test_tree_to_frames.py
@@ -8,7 +8,7 @@ from shnitsel.io import read
     params=[
         'tutorials/test_data/shnitsel/traj_I02.nc',
         'tutorials/test_data/sharc/traj_I01_v3.0_triplets_nacs_socs',
-        'tutorials/test_data/newtonx/test_I01_v2.6',
+        'tutorials/test_data/newtonx/test_pyrazene_v2.6',
     ]
 )
 def tree(request):
@@ -18,11 +18,13 @@ def tree(request):
 
 def test_tree_to_stacked(tree):
     frames = tree.as_stacked
-    assert ('atrajectory' in frames.dims)
-    assert ('trajectory' in frames.dims)
+    assert 'atrajectory' in frames.coords
+    assert 'frame' in frames.dims
+    assert 'trajectory' in frames.dims
 
 
 def test_tree_to_layered(tree):
     layered = tree.as_layered
-    assert ('atrajectory' not in layered.dims)
-    assert ('trajectory' in layered.dims)
+    assert 'atrajectory' not in layered.dims
+    assert 'atrajectory' not in layered.coords
+    assert 'trajectory' in layered.dims
