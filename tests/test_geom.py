65c0ef860afe90cdf47cd3adac216ae5919441f3
Kevin Hoellring <kevin.hoellring@fau.de>
2026-01-18 18:37:41 +0100

    Implement more tests for geo module

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -1,5 +1,13 @@
 from shnitsel.io import read
-from shnitsel.geo.geocalc import get_bats
+from shnitsel.geo.geocalc import (
+    get_bats,
+    get_angles,
+    get_dihedrals,
+    get_distances,
+    get_pyramidalization,
+    get_max_chromophor_BLA,
+)
+from shnitsel.filtering.structure_selection import StructureSelection
 
 from pytest import fixture
 
@@ -9,12 +17,54 @@ from pytest import fixture
         ('tutorials/tut_data/traj_I02.nc', -1),
     ]
 )
-def frames(request):
+def db(request):
     path, charge = request.param
     db = read(path).set_charge(charge)
-    return db.as_frames()
+    return db
 
 
-def test_get_bats(frames):
-    res = get_bats(frames['atXYZ'])
-    assert 'descriptor' in res.dims
+@fixture()
+def selection(db):
+    return StructureSelection.init_from_dataset(
+        list(db.collect_datA())[0], ['bonds', 'angles', 'dihedrals', 'pyramids']
+    )
+
+
+def test_get_bats(db):
+    res = get_bats(db)
+    assert all('descriptor' in x.dims for x in res.collect_data())
+
+
+def test_get_bats_selection(db, selection):
+    res = get_bats(db, selection)
+    assert all('descriptor' in x.dims for x in res.collect_data())
+
+
+def test_get_bats_all(db):
+    res = get_bats(db, default_features=['bonds', 'angles', 'dihedrals', 'pyramids'])
+    assert all('descriptor' in x.dims for x in res.collect_data())
+
+
+def test_get_distances(db):
+    res = get_distances(db)
+    assert all('descriptor' in x.dims for x in res.collect_data())
+
+
+def test_get_angles(db):
+    res = get_angles(db)
+    assert all('descriptor' in x.dims for x in res.collect_data())
+
+
+def test_get_dihedrals(db):
+    res = get_dihedrals(db)
+    assert all('descriptor' in x.dims for x in res.collect_data())
+
+
+def test_get_pyramidalization(db):
+    res = get_pyramidalization(db)
+    assert all('descriptor' in x.dims for x in res.collect_data())
+
+
+def test_get_max_chromophor_BLA(db):
+    res = get_max_chromophor_BLA(db)
+    assert all('descriptor' in x.dims for x in res.collect_data())
