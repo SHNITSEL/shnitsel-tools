04e5ca455c5cabb38f74dfde30147a16fe0a11a5
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2026-01-18 20:32:25 +0100

    Prepare `Filtration` class for plotting

--- shnitsel/clean/filtration_class.py
+++ shnitsel/clean/filtration_class.py
@@ -1,9 +1,11 @@
-from shnitsel.data.multi_indices import ensure_unstacked
 from functools import cached_property
+from typing import Sequence
+
+import numpy as np
 import xarray as xr
 
 from shnitsel.data.dataset_containers import wrap_dataset, ShnitselDataset
-from shnitsel.data.multi_indices import sel_trajs
+from shnitsel.data.multi_indices import ensure_unstacked, sel_trajs
 
 class Filtration:
     def __init__(self, subject, filtranda=None):
@@ -37,6 +39,9 @@ class Filtration:
             else ''
         )
 
+        # FIXME: This may not generalize:
+        self.unstacked_trajectory_dim = self.trajectory_groupable
+
     @cached_property
     def noncumulative_mask(self):
         return self.filtranda < self.thresholds
@@ -59,6 +64,15 @@ class Filtration:
             res = self.noncumulative_mask.all(self.leading_dim)
         return res.astype(bool)
 
+    # NOTE (thevro): I'm hesitant to make this a property, because unstacking can take time
+    def get_unstacked_dataset(self):
+        unstacked, _ = ensure_unstacked(self.subject_original)
+        return unstacked
+
+    def get_unstacked_filtranda(self):
+        unstacked_filtranda, _ = ensure_unstacked(self.filtranda)
+        return unstacked_filtranda
+
     def truncate(self):
         """Perform a truncation, i.e. cut off the trajectory at its last continuously valid frame from the begining."""
         filter_mask_all_criteria = self.cumulative_mask.all('criterion')
@@ -71,10 +85,7 @@ class Filtration:
     def omit(self):
         all_critera_fulfilled = self.good_throughout.all('criterion')
         if self.trajectory_groupable:
-            # x = self.trajectory_groupable
-            # mask = all_critera_fulfilled.sel({x: self.subject_dataset.coords[x]})
             return type(self.subject_original)(
-                # self.subject_dataset.isel({self.leading_dim: mask.data})
                 sel_trajs(self.subject_dataset, all_critera_fulfilled)
             )
 
@@ -83,19 +94,47 @@ class Filtration:
 
     def transect(self, cutoff_time):
         indexer = {"time": slice(float(cutoff_time))}
-        unstacked, _ = ensure_unstacked(self.subject_original)
-        unstacked_filtranda, _ = ensure_unstacked(self.filtranda)
-        sliced = unstacked.loc[indexer]
-        sliced_filtranda = unstacked_filtranda.loc[indexer]
+        sliced = self.get_unstacked_dataset().loc[indexer]
+        sliced_filtranda = self.get_unstacked_filtranda().loc[indexer]
         return Filtration(sliced, sliced_filtranda).omit()
+
+    def get_unstacked_cumulative_maxima(self):
+        return xr.apply_ufunc(
+            np.maximum.accumulate,
+            self.get_unstacked_filtranda(),
+            input_core_dims=[['time']],
+            output_core_dims=[['time']],
+            kwargs={'axis': -1},
+        )
+
+    def cum_max_quantiles(
+        self, quantiles: Sequence[float] | None = None
+    ) -> xr.DataArray:
+        if quantiles is None:
+            quantiles = [0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 1]
+        return self.get_unstacked_cumulative_maxima().quantile(
+            quantiles, self.unstacked_trajectory_dim
+        )
+
+    def get_unstacked_cumulative_mask(self):
+        unstacked_mask, _ = ensure_unstacked(self.cumulative_mask)
+        return unstacked_mask
+
+    def get_independent_populations(self):
+        mask = (
+            self.get_unstacked_cumulative_mask()
+            .to_dataset('criterion')
+            .assign({'total_population': mask.coords['is_frame']})
+            .to_dataarray('criterion')
+        )
+        return mask  # FIXME
+
+    def plot_thresholds(self): ...
+
+    def plot_populations(self): ...
 
     # NOTE (thevro): The following is not a property because it can fail if there is no time dimension
     # def good_upto(self):
     #     from shnitsel.clean.common import true_upto
 
     #     return true_upto(self.cumulative_mask)
-
-
-    def plot_thresholds(self): ...
-
-    def plot_populations(self): ...
