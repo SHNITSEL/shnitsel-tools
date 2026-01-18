960607077fe11dc554bfe861a8438911d0b3209f
Kevin Hoellring <kevin.hoellring@fau.de>
2026-01-18 21:25:17 +0100

    Udpate accessor test

--- tests/test_accessors.py
+++ tests/test_accessors.py
@@ -9,7 +9,7 @@ class TestAccessors:
 
     @pytest.fixture(
         params=[
-            ('tutorials/tut_data/traj_I02.nc', 1),
+            ('./tutorials/test_data/shnitsel/traj_I02.nc', 1),
         ]
     )
     def ds(self, request):
@@ -21,7 +21,9 @@ class TestAccessors:
         res = db.as_stacked
         return res
 
-    def test_da_accessors(self, ds, subtests):
+    # TODO: Subtests is a feature of pytest 9, which we do not list as requirement
+    # def test_da_accessors(self, ds, subtests):
+    def test_da_accessors(self, ds):
         kws = {
             'norm': dict(),
             'subtract_combinations': dict(),
@@ -73,24 +75,27 @@ class TestAccessors:
         }
         for var_name, da in ds.data_vars.items():
             for method_name in da.st.suitable:
-                with subtests.test(
-                    f"ds['{var_name}'].st.{method_name}",
-                    var_name=var_name,
-                    method_name=method_name,
-                ):
-                    assert hasattr(da.st, method_name)
-                    # if method_name in kws:
-                    #     getattr(da.st, method_name)(**kws[method_name])
+                # with subtests.test(
+                #     f"ds['{var_name}'].st.{method_name}",
+                #     var_name=var_name,
+                #     method_name=method_name,
+                # ):
+                assert hasattr(da.st, method_name)
+                # if method_name in kws:
+                #     getattr(da.st, method_name)(**kws[method_name])
 
-    def test_ds_accessors(self, ds, subtests):
+    # TODO: Subtests is a feature of pytest 9, which we do not list as requirement
+    # def test_ds_accessors(self, ds, subtests):
+    # TODO: FIXME: Multi-index selectors/helpers are broken
+    def test_ds_accessors(self, ds):
         kws = {
-            'pca_and_hops': dict(mean=False),
-            'flatten_levels': dict(idx_name='frame'),
+            'pca_and_hops': dict(center_mean=False),
+            'flatten_levels': dict(idx_name='frame', levels=['time']),
             'expand_midx': dict(midx_name='frame', level_name='compound', value=''),
             # 'assign_levels': dict(),
-            'mgroupby': dict(levels=['trajid', 'time']),
+            'mgroupby': dict(levels=['atrajectory', 'time']),
             # 'msel': dict(),
-            'sel_trajs': dict(trajids=[]),
+            'sel_trajs': dict(trajids_or_mask=[1]),
             'stack_trajs': dict(),
             'energy_filtranda': dict(),
             'sanity_check': dict(),
@@ -109,7 +114,8 @@ class TestAccessors:
             'TrajSelector',  # cf. FrameSelector
         ]
         for method_name in ds.st.suitable:
-            with subtests.test(f"ds.st.{method_name}", method_name=method_name):
-                assert hasattr(ds.st, method_name)
-                if method_name not in blacklist:
-                    getattr(ds.st, method_name)(**kws[method_name])
+            # with subtests.test(f"ds.st.{method_name}", method_name=method_name):
+            assert hasattr(ds.st, method_name)
+            if method_name in kws and method_name not in blacklist:
+                print(method_name)
+                getattr(ds.st, method_name)(**kws[method_name])
