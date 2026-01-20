2e7833a4c3dcc06de0ffde3f74937fc238a0e754
Kevin Höllring <kevin.hoellring@fau.de>
2026-01-20 14:42:02 +0100

    Fix documentation of algebra methods

--- shnitsel/geo/geocalc_/algebra.py
+++ shnitsel/geo/geocalc_/algebra.py
@@ -8,11 +8,15 @@ import numpy as np
 def normalize(a: DataArrayOrVar, dim='direction') -> DataArrayOrVar:
     """Return vectors normalized along the `dim` dimension.
 
-    Args:
-        a (xr.DataArray | xr.Variable): The data array to normalize along the `dim` dimension.
+    Parameters
+    ----------
+    a : xr.DataArray | xr.Variable
+        The data array to normalize along the `dim` dimension.
 
-    Returns:
-        xr.DataArray | xr.Variable: Resulting DataArray after calculation of the noramlized vectors in the dimension `dim`.
+    Returns
+    -------
+    xr.DataArray | xr.Variable
+        Resulting DataArray after calculation of the noramlized vectors in the dimension `dim`.
     """
     return a / norm(a, dim=dim)
 
@@ -20,11 +24,15 @@ def normalize(a: DataArrayOrVar, dim='direction') -> DataArrayOrVar:
 def dnorm(a: DataArrayOrVar) -> DataArrayOrVar:
     """Calculate the norm along the `direction` dimension. All other dimensions are maintaned
 
-    Args:
-        a (xr.DataArray | xr.Variable): The data array to perform the norming on
+    Parameters
+    ----------
+    a : xr.DataArray | xr.Variable
+        The data array to perform the norming on
 
-    Returns:
-        xr.DataArray | xr.Variable: Resulting dataarray after calculation of the norm in the dimension `direction.
+    Returns
+    -------
+    xr.DataArray | xr.Variable 
+        Resulting dataarray after calculation of the norm in the dimension `direction.
     """
     return norm(a, dim='direction')
 
@@ -32,12 +40,17 @@ def dnorm(a: DataArrayOrVar) -> DataArrayOrVar:
 def dcross(a: DataArrayOrVar, b: DataArrayOrVar) -> DataArrayOrVar:
     """Generalized cross vector product in the dimension of `direction`.
 
-    Args:
-        a (xr.DataArray | xr.Variable): The first array to use for the binary operation
-        b (xr.DataArray | xr.Variable): The second array to use for the binary operation
+    Parameters
+    ----------
+    a : xr.DataArray | xr.Variable
+        The first array to use for the binary operation
+    b : xr.DataArray | xr.Variable
+        The second array to use for the binary operation
 
-    Returns:
-        xr.DataArray | xr.Variable: The resulting array of the cross-product
+    Returns
+    -------
+    xr.DataArray | xr.Variable
+        The resulting array of the cross-product
     """
     return xr.cross(a, b, dim='direction')  # type: ignore # Type should be same as input
 
@@ -45,12 +58,17 @@ def dcross(a: DataArrayOrVar, b: DataArrayOrVar) -> DataArrayOrVar:
 def ddot(a: DataArrayOrVar, b: DataArrayOrVar) -> DataArrayOrVar:
     """Dot product in the dimension of `direction`.
 
-    Args:
-        a (xr.DataArray | xr.Variable): The first array to use for the binary operation
-        b (xr.DataArray | xr.Variable): The second array to use for the binary operation
+    Parameters
+    ----------
+    a : xr.DataArray | xr.Variable 
+        The first array to use for the binary operation
+    b : xr.DataArray | xr.Variable 
+        The second array to use for the binary operation
 
-    Returns:
-        xr.DataArray | xr.Variable: The resulting array of the dot-product still retaining all other dimensions except `direction`.
+    Returns
+    -------
+    xr.DataArray | xr.Variable
+        The resulting array of the dot-product still retaining all other dimensions except `direction`.
     """
     return xr.dot(a, b, dim='direction')
 
@@ -58,12 +76,17 @@ def ddot(a: DataArrayOrVar, b: DataArrayOrVar) -> DataArrayOrVar:
 def angle_(a: DataArrayOrVar, b: DataArrayOrVar) -> DataArrayOrVar:
     """Helper function to calculate the angle between the entries in a and b based on their coordinates in the `direction` dimension.
 
-    Args:
-        a (xr.DataArray | xr.Variable): The first array to use for the binary operation
-        b (xr.DataArray | xr.Variable): The second array to use for the binary operation
+    Parameters
+    ----------
+    a : xr.DataArray | xr.Variable
+        The first array to use for the binary operation
+    b : xr.DataArray | xr.Variable
+        The second array to use for the binary operation
 
-    Returns:
-        xr.DataArray | xr.Variable: The resulting array of the angle calculation still retaining all other dimensions except `direction`.
+    Returns
+    -------
+    xr.DataArray | xr.Variable
+        The resulting array of the angle calculation still retaining all other dimensions except `direction`.
     """
 
     cos_theta = ddot(a, b) / (dnorm(a) * dnorm(b))
@@ -77,12 +100,17 @@ def angle_cos_sin_(
     """
     Returns the cosine and sine of the angle between two vectors
 
-    Args:
-        a (DataArrayOrVar): First vector
-        b (DataArrayOrVar): Second vector
+    Parameters
+    ----------
+    a : DataArrayOrVar
+        First vector
+    b : DataArrayOrVar
+        Second vector
 
-    Returns:
-        tuple[DataArrayOrVar, DataArrayOrVar]: First the vector of cosines, second the vector of sines.
+    Returns
+    -------
+    tuple[DataArrayOrVar, DataArrayOrVar]
+        First the vector of cosines, second the vector of sines.
     """
     prod = dnorm(a) * dnorm(b)
     return (
@@ -100,12 +128,18 @@ def normal(
 
     The normal vector will be calculated based on the position in `direction` dimension.
 
-    Args:
-        a (xr.DataArray | xr.Variable): The first array to use for the ternary operation
-        b (xr.DataArray | xr.Variable): The second array to use for the ternary operation
-        c (xr.DataArray | xr.Variable): The third array to use for the ternary operation
+    Parameters
+    ----------
+    a : xr.DataArray | xr.Variable
+        The first array to use for the ternary operation
+    b : xr.DataArray | xr.Variable
+        The second array to use for the ternary operation
+    c : xr.DataArray | xr.Variable
+        The third array to use for the ternary operation
 
-    Returns:
-        xr.DataArray | xr.Variable: An array with all dimensions equal to those of a, b, and c but holding normal vectors along the `direction` dimension.
+    Returns
+    -------
+    xr.DataArray | xr.Variable
+        An array with all dimensions equal to those of a, b, and c but holding normal vectors along the `direction` dimension.
     """
     return dcross(a - b, c - b)
