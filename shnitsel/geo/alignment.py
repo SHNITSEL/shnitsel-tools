474ee71900ce84e3eedc1014c8d4929348a101f9
Kevin Höllring <kevin.hoellring@fau.de>
2026-01-28 09:51:04 +0100

    Tweak alignment documentation

--- shnitsel/geo/alignment.py
+++ shnitsel/geo/alignment.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Literal
+from typing import Iterable, Literal, Sequence
 from shnitsel._contracts import needs
 import xarray as xr
 import numpy as np
@@ -34,10 +34,14 @@ def get_centered_geometry(atXYZ: AtXYZ, by_mass: Literal[False] = False) -> AtXY
     return atXYZ - atXYZ.mean('atom')
 
 
-def rotational_procrustes_(
-    A: np.ndarray, B: np.ndarray, weight: Iterable[float] | None = None
+def rotational_procrustes_np(
+    A: np.ndarray, B: np.ndarray, weight: Sequence[float] | None = None
 ) -> np.ndarray:
     """Rotationally align the geometrie(s) in A to the single geometry in B.
+
+    This helper function is specifically tailored to work directly on numpy arrays
+    in contrast to its namesake `rotational_procrustes()`, which accepts
+    `xarray.DataArray` parameters.
 
     Parameters
     ----------
@@ -47,7 +51,7 @@ def rotational_procrustes_(
     B : np.ndarray
         The reference geometry with shape
         ``(n_points, n_coordinates)``
-    weight : Iterable[float], optional
+    weight : Sequence[float], optional
         How much importance should be given to the alignment of
         each point, by default equal importance
 
@@ -55,6 +59,29 @@ def rotational_procrustes_(
     -------
     np.ndarray
         An array with the same shape as A
+
+    Notes
+    -----
+    We are solving the following minimization problem for each geometry `A(i)`:
+    .. math::
+        \\min_R \\Vert A(i)R - B\\Vert^2_F
+
+    For this, we calculte the weighted cross-covariance matrix:
+    We are solving the minimization problem
+    .. math::
+        C = A^T B
+
+    And then we find the SVD
+    .. math::
+        C = U \\Sigma V^T
+
+    Which provides us with the optimum rotation:
+    .. math::
+        R = V U^T
+
+    If the resulting R has negative determinant, the rotation is instead
+    a mirroring operation and we invert the sign of the last column of vt
+    to restore rotational properties.
     """
     from scipy.linalg import svd
 
@@ -82,14 +109,14 @@ def rotational_procrustes(
     B: xr.DataArray,
     dim0: str = 'atom',
     dim1: str = 'direction',
-    weight: Iterable[float] | None = None,
+    weight: Sequence[float] | None = None,
 ) -> xr.DataArray:
-    """Rotationally align the geometrie(s) in A to the single geometry in B.
+    """Rotationally align the geometry or geometries in A to the single geometry in B.
 
     Parameters
     ----------
     A : xr.DataArray
-        The geometries to process
+        The (optionally multiple) geometries to process
     B : xr.DataArray
         The reference geometry
     dim0 : str, optional
@@ -98,7 +125,7 @@ def rotational_procrustes(
     dim1 : str, optional
         The name of the dimension over the coordinates of the aforementioned
         points; must be present in ``A`` and ``B`; by default 'direction'
-    weight : Iterable[float], optional
+    weight : Sequence[float], optional
         How much importance should be given to the alignment of
         each point (atom), by default equal importance
 
@@ -108,7 +135,7 @@ def rotational_procrustes(
         An xr.DataArray with the same shape as ``A`` but with entries aligned to the overall geometry of ``B``
     """
     return xr.apply_ufunc(
-        rotational_procrustes_,
+        rotational_procrustes_np,
         A,
         B,
         input_core_dims=[[dim0, dim1], [dim0, dim1]],
@@ -123,7 +150,10 @@ def kabsch(
     reference_or_indexers: xr.DataArray | dict | None = None,
     **indexers_kwargs,
 ) -> xr.DataArray:
-    """Rotationally align the molecular geometries in ``atXYZ`` to a single molecular geometry
+    """Rotationally align the molecular geometries in ``atXYZ`` to a single molecular geometry.
+
+    If no `reference_or_indexers` argument (or the `indexers_kwargs` option) is passed,
+    this function will try to use the first frame or first timestep in `atXYZ` as a reference.
 
     Parameters
     ----------
@@ -133,6 +163,8 @@ def kabsch(
     reference_or_indexers : xr.DataArray | dict, optional
         Either a reference geometry (with dims 'atom', 'direction')
         or an indexer dictionary which will be passed to ``atXYZ.sel()``
+        to indetify a single geometry in the `atXYZ` parameter to
+        use as a reference point.
 
     **indexer_kwargs
         The keyword-argument form of the indexer to be passed to ``atXYZ.sel()``
@@ -157,11 +189,20 @@ def kabsch(
         reference = atXYZ.sel(indexers_kwargs)
     elif 'frame' in atXYZ.dims:
         reference = atXYZ.isel(frame=0)
+    elif 'time' in atXYZ.dims:
+        reference = atXYZ.isel(time=0)
     else:
-        raise ValueError("Please specify a reference geometry")
+        raise ValueError(
+            "Could not retrieve reference geometry. Please specify a reference geometry."
+        )
 
     # TODO: is it ever necessary to center the molecule?
     # If so, should this always be done using the physical center-of-mass,
     # or is it ever appropriate to use the unweighted mean of points?
+
+    # TODO: Kevin: As far as I understand the theory, you first center all geometries
+    # Then you remove all scales via the mean RMSD scale and then you apply rotational procustes.
+    # Based on the SVD, the S matrix should be able to figure out the scale, but we do not
+    # Use that for rotation. May be that the centering is the key part that is missing.
 
     return rotational_procrustes(atXYZ, reference)
