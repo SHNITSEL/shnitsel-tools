9df5d3b8ba4469c1d17e9e2556a5eeb74c84c5b1
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2026-01-29 12:26:17 +0100

    Add 'time_cord` param to `timeplot()`

--- shnitsel/vis/plot/time.py
+++ shnitsel/vis/plot/time.py
@@ -25,7 +25,7 @@ def _set_axes(data, ax=None):
     ax.set_xlabel(xlabel)
     return ax
 
-def plot_single(data, ax=None):
+def plot_single(data, ax=None, time_coord='time'):
     """Plot some property of a single trajectory over time
 
     Parameters
@@ -62,16 +62,17 @@ def plot_single(data, ax=None):
     for _, sdata in data.groupby(groupby):
         sdata = sdata.squeeze(groupby)
         # c = sdata['_color'].item()
-        line2d = ax.plot(sdata['time'], sdata, lw=0.5)#, c=c)
+        line2d = ax.plot(sdata[time_coord], sdata, lw=0.5)  # , c=c)
         ax.text(
-            sdata['time'][-1],
+            sdata[time_coord][-1],
             sdata[-1],
             sdata.coords[coord_name].item(),
             va='center',
-            c=line2d[0].get_color())
+            c=line2d[0].get_color(),
+        )
     return _set_axes(data, ax)
 
-def plot_ci(data, ax=None):
+def plot_ci(data, ax=None, time_coord='time'):
     """Plot some property of trajectories over time, aggregated as means and confidence intervals
 
     Parameters
@@ -104,26 +105,26 @@ def plot_ci(data, ax=None):
     else:
         # TODO FIXME The expand_dims and squeeze steps shouldn't be necessary
         ci = time_grouped_confidence_interval(data.expand_dims('state')).squeeze('state')
-        ax.fill_between('time', 'upper', 'lower', data=ci, alpha=0.3)
-        line2d = ax.plot('time', 'mean', data=ci, lw=0.8)
+        ax.fill_between(time_coord, 'upper', 'lower', data=ci, alpha=0.3)
+        line2d = ax.plot(time_coord, 'mean', data=ci, lw=0.8)
         return _set_axes(data, ax)
         
 
     ci = time_grouped_confidence_interval(data)
     for _, sdata in ci.groupby(dim):
         sdata = sdata.squeeze(dim)
-        ax.fill_between('time', 'upper', 'lower', data=sdata, alpha=0.3)
-        line2d = ax.plot('time', 'mean', data=sdata, lw=0.8)
+        ax.fill_between(time_coord, 'upper', 'lower', data=sdata, alpha=0.3)
+        line2d = ax.plot(time_coord, 'mean', data=sdata, lw=0.8)
         ax.text(
-            sdata['time'][-1],
+            sdata[time_coord][-1],
             sdata['mean'][-1],
             sdata.coords[coord_name].item(),
             va='center',
-            c=line2d[0].get_color()
+            c=line2d[0].get_color(),
         )
     return _set_axes(data, ax)
 
-def plot_many(data, ax=None):
+def plot_many(data, ax=None, time_coord='time'):
     """Plot some property of trajectories over time as thin lines;
     state or statecomb is indicated by colour
 
@@ -160,7 +161,7 @@ def plot_many(data, ax=None):
         dim = 'tmp'
         groupby = [(None, data)]
         for _, traj in data.groupby('atrajectory'):
-            ax.plot(traj['time'], traj, lw=0.5, c='k')
+            ax.plot(traj[time_coord], traj, lw=0.5, c='k')
         return _set_axes(data, ax)
     
     colors = iter(plt.get_cmap('tab10').colors)
@@ -169,11 +170,11 @@ def plot_many(data, ax=None):
         label = sdata.coords[coord_name].item()
         c = next(colors)
         for _, traj in sdata.groupby('atrajectory'):
-            ax.plot(traj['time'], traj, lw=0.5, label=label, c=c)
+            ax.plot(traj[time_coord], traj, lw=0.5, label=label, c=c)
     # TODO: legend
     return _set_axes(data, ax)
 
-def plot_shaded(data, ax):
+def plot_shaded(data, ax, time_coord='time'):
     """Plot some property of trajectories over time, aggregated by using colour
     to show how many overlap at a given point
 
@@ -211,7 +212,7 @@ def plot_shaded(data, ax):
     x = []
     y = []
     for _, traj in data.groupby('atrajectory'):
-        x.append(traj.time.values)
+        x.append(traj.coords[time_coord].values)
         y.append(traj.values)
     df = pd.DataFrame({
         'x': pd.array(x, dtype='Ragged[float64]'),
@@ -232,6 +233,7 @@ def timeplot(
     ax: plt.Axes | None = None,
     trajs: Literal['ci', 'shade', 'conv', None] = None,
     sep: bool = False,
+    time_coord='time',
 ):
     """Plot some property of one or many trajectories over time,
     possibly aggregating over trajectories, and distinguishing
@@ -299,23 +301,25 @@ def timeplot(
         for (_, sdata), ax in zip(data.groupby(state_dim), axs):
             ax.set_title(sdata.coords[coord_name].item())
             sdata = sdata.squeeze(state_dim)
-            res.append(timeplot(sdata, ax=ax, trajs=trajs, sep=False))
+            res.append(
+                timeplot(sdata, ax=ax, trajs=trajs, sep=False, time_coord=time_coord)
+            )
         return res
 
     if 'trajid' not in data.coords and 'atrajectory' not in data.coords:
         assert trajs is None
-        return plot_single(data, ax)
+        return plot_single(data, ax, time_coord=time_coord)
     if trajs == 'ci':
-        return plot_ci(data, ax)
+        return plot_ci(data, ax, time_coord=time_coord)
     elif trajs == 'shade':
-        return plot_shaded(data, ax)
+        return plot_shaded(data, ax, time_coord=time_coord)
     elif trajs == 'conv':
         raise NotImplementedError(
             "Convolutions are not yet implemented here, "
             "please use xr_broaden_gauss manually"
         )
     elif trajs is None:
-        return plot_many(data, ax)
+        return plot_many(data, ax, time_coord=time_coord)
     else:
         raise ValueError(
             f"`trajs` should be one of 'ci', 'shade' or None, rather than {trajs}"
