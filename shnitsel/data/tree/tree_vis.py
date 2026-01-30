39114cdd9c073ef7dbc1280feb2ed069824776d8
Kevin Höllring <kevin.hoellring@fau.de>
2026-01-30 10:11:44 +0100

    Handle any possible error in to_mol in tree visualization gracefully

--- shnitsel/data/tree/tree_vis.py
+++ shnitsel/data/tree/tree_vis.py
@@ -209,7 +209,7 @@ def datatree_child_repr(node: TreeNode, end: bool = False) -> str:
                 mol_object = default_mol(mol_source)
                 mol = rdkit.Chem.Draw.rdMolDraw2D.MolToSVG(mol_object, width=100, height=60)
                 mol = ''.join(mol.split('\n')[1:])
-            except ValueError:
+            except: #ValueError:
                 # default_mol didn't work
                 mol = ''
     else:
