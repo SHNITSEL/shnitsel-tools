933245bb17b8feaa4349bf395224a7f0fc655459
Kevin Höllring <kevin.hoellring@fau.de>
2026-02-10 10:31:10 +0100

    Clean up highlight_features code

--- shnitsel/rd.py
+++ shnitsel/rd.py
@@ -1,8 +1,9 @@
 """This module contains functions that accept an RDKit.Chem.Mol object;
 but *not* necessarily functions that *return* a Mol object."""
 
-from typing import Literal, Mapping, Sequence, TYPE_CHECKING
+from typing import Iterable, Literal, Mapping, Sequence, TYPE_CHECKING
 
+from matplotlib.colors import Colormap
 import rdkit.Chem as rc
 import rdkit.Chem.rdDetermineBonds  # noqa: F401
 import matplotlib as mpl
@@ -112,8 +113,10 @@ def mol_to_numbered_smiles(mol: rc.Mol) -> str:
 
 def highlight_features(
     mol: rc.Mol,
-    feature_indices: Sequence["FeatureDescriptor"],
+    feature_indices: Sequence["FeatureDescriptor | None"],
     fmt: Literal['png', 'svg'] = 'png',
+    colors: Sequence[tuple] | None = None,
+    cmap: str | Colormap | None = None,
     width=320,
     height=240,
 ):
@@ -123,12 +126,24 @@ def highlight_features(
     ----------
     mol : rc.Mol
         The ``Mol`` object
-    feature_indices : Sequence[FeatureDescriptor]
+    feature_indices : Sequence[FeatureDescriptor | None]
         A list of tuples of indices for various features.
+    fmt : {'png', 'svg'}, default='png'
+        Optional format specifier for the desired image output format.
+    colors : Sequence[tuple], optional
+        Optionally a sequence of colors to plot the features in. If set, does not consider the value in `cmap`.
+        If not provided, will be generated from `cmap`.
+    cmap : str | Colormap, optional
+        Either the name of a `Colormap` or a `Colormap` to use to assign colors to the features if `colors` is not set.
+        If required but not provided defaults to the `rainbow` colormap.
+    width: int, default=320
+        The width of the target graphic in pixels
+    height: int, default=320
+        The height of the target graphic in pixels
 
     Returns
     -------
-        Raw PNG data
+        Raw PNG data or the SVG string depending on the choice of `fmt`.
     """
     if fmt == 'png':
         d = rdkit.Chem.Draw.rdMolDraw2D.MolDraw2DCairo(width, height)
@@ -139,79 +154,100 @@ def highlight_features(
             f"Unsupported format for molecular highlight graph: {fmt=}. Supported are 'png' and 'svg'"
         )
 
-    # colors = iter(mpl.colormaps['tab10'](range(10)))
-    colors = iter(mpl.colormaps['rainbow'](np.linspace(0, 1, len(feature_indices))))
+    plot_colors: Iterable[tuple]
+    if colors is None:
+        if cmap is None:
+            # colors = iter(mpl.colormaps['tab10'](range(10)))
+            plot_colors = iter(
+                mpl.colormaps['rainbow'](np.linspace(0, 1, len(feature_indices)))
+            )
+        else:
+            if isinstance(cmap, str):
+                cmap = mpl.colormaps[cmap]
+            plot_colors = cmap(np.linspace(0, 1, len(feature_indices)))
+    else:
+        assert len(colors) >= len(feature_indices), (
+            "Insufficient number colors for the number of features provided."
+        )
+        plot_colors = colors
+    atom_colors: dict[int, list[tuple[float, float, float]]] = {}
+    bond_colors: dict[int, list[tuple[float, float, float]]] = {}
 
-    acolors: dict[int, list[tuple[float, float, float]]] = {}
-    bonds: dict[int, list[tuple[float, float, float]]] = {}
-    for feature in feature_indices:
-        c = tuple(next(colors))
+    # We expect here that there are sufficient colors for our features
+    for feature, c in zip(feature_indices, plot_colors):
+        if feature is None:
+            continue
+
+        atoms_to_color = []
+
         if isinstance(feature, int):
             # Position
             a = feature
-            if a not in acolors:
-                acolors[a] = []
-            acolors[a].append(c)
+            atoms_to_color = [a]
         elif isinstance(feature, tuple):
-            flen = len(feature)
-            if flen == 2:
-                if isinstance(feature[1], int):
-                    # Bond
-                    a1, a2 = feature
-                    if a1 < 0 or a2 < 0:
-                        continue
-                    if (bond := mol.GetBondBetweenAtoms(a1, a2)) is not None:
-                        bondid = bond.GetIdx()
-                        if bondid not in bonds:
-                            bonds[bondid] = []
-                        bonds[bond.GetIdx()].append(c)
-                    else:
-                        for a in [a1, a2]:
-                            if a not in acolors:
-                                acolors[a] = []
-                            acolors[a].append(c)
-                elif isinstance(feature[1], tuple):
+            match feature:
+                case a1, (a2, a3, a4):
                     # pyramid
-                    a1, (a2, a3, a4) = feature
                     if a1 < 0 or a2 < 0 or a3 < 0 or a4 < 0:
                         continue
                     # Mark bonds
                     for other in (a2, a3, a4):
                         if (bond := mol.GetBondBetweenAtoms(a1, other)) is not None:
                             bondid = bond.GetIdx()
-                            if bondid not in bonds:
-                                bonds[bondid] = []
-                            bonds[bond.GetIdx()].append(c)
+                            if bondid not in bond_colors:
+                                bond_colors[bondid] = []
+                            bond_colors[bond.GetIdx()].append(c)
                     # Mark all atoms
-                    for a in [a1, a2, a3, a4]:
-                        if a not in acolors:
-                            acolors[a] = []
-                        acolors[a].append(c)
-            elif flen == 3 or flen == 4:
-                # angle or dihedral
-                # Mark bonds
-                if not all(x >= 0 for x in feature):
-                    continue
+                    atoms_to_color = [a1, a2, a3, a4]
+                case (a1, a2):
+                    # Bond
+                    a1, a2 = feature
+                    if a1 < 0 or a2 < 0:
+                        continue
 
-                for i in range(flen - 1):
-                    a1, a2 = feature[i], feature[i + 1]
                     if (bond := mol.GetBondBetweenAtoms(a1, a2)) is not None:
                         bondid = bond.GetIdx()
-                        if bondid not in bonds:
-                            bonds[bondid] = []
-                        bonds[bond.GetIdx()].append(c)
+                        if bondid not in bond_colors:
+                            bond_colors[bondid] = []
+                        bond_colors[bond.GetIdx()].append(c)
+                    else:
+                        atoms_to_color = [a1, a2]
+                case _:
+                    # angle or dihedral
+                    # Mark bonds
+                    if not all(x >= 0 for x in feature):
+                        continue
 
-                # Mark all atoms
-                for a in feature:
-                    if a not in acolors:
-                        acolors[a] = []
-                    acolors[a].append(c)
+                    flen = len(feature)
+
+                    for i in range(flen - 1):
+                        a1, a2 = feature[i], feature[i + 1]
+                        if isinstance(a1, int) and isinstance(a2, int):
+                            if (bond := mol.GetBondBetweenAtoms(a1, a2)) is not None:
+                                bondid = bond.GetIdx()
+                                if bondid not in bond_colors:
+                                    bond_colors[bondid] = []
+                                bond_colors[bond.GetIdx()].append(c)
+
+                    # Mark all atoms
+                    atoms_to_color = [x for x in feature if isinstance(x, int)]
+
+        # Mark all atoms
+        for a in atoms_to_color:
+            if a < 0:
+                # Ignore negative entries
+                continue
+
+            if a not in atom_colors:
+                atom_colors[a] = []
+            atom_colors[a].append(c)
 
     # d.drawOptions().fillHighlights = False
     d.drawOptions().setBackgroundColour((0.8, 0.8, 0.8, 0.5))
     d.drawOptions().padding = 0
+    # TODO: FIXME: We seem to have double indices because of this
     d.drawOptions().addAtomIndices = True
 
-    d.DrawMoleculeWithHighlights(mol, '', acolors, bonds, {}, {}, -1)
+    d.DrawMoleculeWithHighlights(mol, '', atom_colors, bond_colors, {}, {}, -1)
     d.FinishDrawing()
     return d.GetDrawingText()
