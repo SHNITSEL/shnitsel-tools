eda1fce12700c14095b01f984f6d30df601d4e61
Kevin Hoellring <kevin.hoellring@fau.de>
2026-02-18 15:55:45 +0100

    Simplify grouping code in datasheet

--- shnitsel/vis/datasheet/datasheet.py
+++ shnitsel/vis/datasheet/datasheet.py
@@ -142,26 +142,21 @@ class Datasheet:
                 ), (
                     "Grouping of the provided ShnitselDB did not yield any result. Please make sure your database is well formed and contains data."
                 )
-
-                tree_res_concat: TreeNode[Any, ShnitselDataset | xr.Dataset] | None = (
-                    grouped_data.map_filtered_nodes(
-                        lambda n: isinstance(n, DataGroup) and n.is_flat_group,
-                        lambda n: n.construct_copy(
-                            children={'agg': DataLeaf(data=n.as_stacked)}
-                        ),
-                        dtype=Frames,
-                    )
+                tmp_tree = grouped_data.map_filtered_nodes(
+                    lambda n: isinstance(n, DataGroup) and n.is_flat_group,
+                    lambda n: n.construct_copy(
+                        children={
+                            'agg': DataLeaf(name='agg', data=(n.path, n.as_stacked))
+                        }
+                    ),
+                    dtype=tuple[str, Frames | xr.Dataset],
                 )
-                assert tree_res_concat is not None, (
+                assert tmp_tree is not None, (
                     "Aggregation of ShnitselDB yielded None. Please provide a database with data."
                 )
 
                 datasheet_groups: list[tuple[str, Frames | xr.Dataset]] = list(
-                    tree_res_concat.map_filtered_nodes(
-                        lambda n: n.is_leaf,
-                        lambda n: DataLeaf(name="tmp", data=(n.path, n.data)),
-                        tuple,
-                    ).collect_data()
+                    tmp_tree.collect_data()
                 )
 
                 for name, traj in datasheet_groups:
