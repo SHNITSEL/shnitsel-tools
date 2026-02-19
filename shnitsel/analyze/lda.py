715082dd408a1035ec31bf7eeeb828c33032a610
Kevin Höllring <kevin.hoellring@fau.de>
2026-02-19 13:52:49 +0100

    Input sanitization of LDA

--- shnitsel/analyze/lda.py
+++ shnitsel/analyze/lda.py
@@ -114,7 +114,11 @@ class LDAResult(
 
 # TODO: FIXME: We have no example for how this is used, so documentation is purely guesswork.
 def lda(
-    dataset: xr.Dataset, dim: str, cats: str | xr.DataArray, n_components: int = 2
+    data: xr.Dataset | xr.DataArray,
+    categories: str | xr.DataArray,
+    dim: str | None = None,
+    features: str | None = None,
+    n_components: int = 2,
 ) -> xr.DataArray:
     """Linear discriminant analysis performed on the data in `data_array` along `dim` in a total of `n_components
 
@@ -136,61 +140,87 @@ def lda(
         The results of the LDA as a DataArray with the categories written to a variables if they weren't there before.
     """
 
-    if isinstance(cats, str):
-        cats_name = cats
-        cats = dataset[cats]
-        # TODO: FIXME: I assume we need to delete the cats entry in the dataset here?
+    input_features: xr.DataArray
+    category_flags: xr.DataArray
+
+    if dim is not None:
+        data = data.transpose(..., dim)
+
+    if isinstance(data, xr.Dataset):
+        assert features is not None and (
+            features in data.data_vars or features in data.coords
+        ), (
+            "If provided an `xarray.Dataset` as `data` argument, the `features` key must be set to specify which data in the dataset to use."
+        )
+
+        input_features = data[features]
+
+        if isinstance(categories, str):
+            assert features is not None and (
+                features in input_features.data_vars
+                or features in input_features.coords
+            ), (
+                "If provided an `xarray.Dataset` as `data` argument, the `features` key must be set to specify which data in the dataset to use."
+            )
+            cats_name = categories
+            category_flags = data[categories]
+            # TODO: FIXME: I assume we need to delete the cats entry in the dataset here?
+        else:
+            cats_name = categories.name
+            category_flags = categories
     else:
-        cats_name = cats.name
+        input_features = data
 
-    dataset = dataset.transpose(..., dim)
-
-    # Scale all features/variables in the dataset to be in 0.0-1.0 range
-    scaled = xr.apply_ufunc(
-        MinMaxScaler().fit_transform,
-    )
-    lda_object = sk_LDA(n_components=n_components)
+        if isinstance(categories, str):
+            assert categories is not None and (
+                features in input_features.data_vars
+                or features in input_features.coords
+            ), (
+                f"The `{categories=}` key must be set to specify which data in the data source to use. "
+                "Was not found in data parameter. Please provide the categories directly as an array of the same size as `data`."
+            )
+            cats_name = categories
+            category_flags = data[categories]
+        else:
+            cats_name = categories.name
+            category_flags = categories
 
     # TODO: For the fit, we may need to apply a different approach than for transform. The category data is only needed for fit, not for transform.
     scaler = MinMaxScaler()
     lda_object = sk_LDA(n_components=n_components)
-    scaler.fit()
+    # Fit the scalers first and get scaled inputs
+    scaled_inputs = scaler.fit_transform(input_features.values)
 
-    pipeline = Pipeline([('scaler', scaler), ('lda', lda_object)])
+    # Fit with category flags separately
+    fitted_lda = lda_object.fit(X=scaled_inputs, y=category_flags)
 
-    lda_res: xr.DataArray = xr.apply_ufunc(
-        pipeline.fit_transform,
+    pipeline = Pipeline([('scaler', scaler), ('lda', fitted_lda)])
+
+    projected_inputs: xr.DataArray = xr.apply_ufunc(
+        pipeline.transform,
         data,
         input_core_dims=[[dim]],
         output_core_dims=[['scaling']],
     )
 
-    def fit_transform(X):
-        # cats: nonlocal
-        return lda_object.fit_transform(X=X, y=cats)
+    # TODO: FIXME: Build the LDA result and return that.
 
-    lda_res: xr.DataArray = xr.apply_ufunc(
-        fit_transform,
-        scaled,
-        input_core_dims=[[dim]],
-        output_core_dims=[['PC']],
-    )
-
-    lda_res[cats_name] = cats
+    projected_inputs[cats_name] = category_flags
 
     scalings = xr.DataArray(
         lda_object.scalings_,
         dims=(dim, 'scaling'),
-        coords={dim: dataset.coords[dim], 'scaling': [i for i in range(n_components)]},
+        # TODO: FIXME: This is again lacking all other coordinates along the `dim` dimension.
+        coords={dim: data.coords[dim], 'scaling': [i for i in range(n_components)]},
     )
 
     if _state.DATAARRAY_ACCESSOR_REGISTERED:
         # TODO: FIXME: Add some explanation of this magic
-        accessor_object = getattr(lda_res, _state.DATAARRAY_ACCESSOR_NAME)
+        accessor_object = getattr(projected_inputs, _state.DATAARRAY_ACCESSOR_NAME)
         accessor_object.scalings = scalings
         accessor_object.lda_object = lda_object
 
-    return lda_res
+    return projected_inputs
 
 
 # Alternative names for the analysis function
