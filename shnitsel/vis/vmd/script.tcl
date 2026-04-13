665cdf687e326f79dc7cc645c2c847e14fd22088
Theodor Everley Röhrkasten <theoeverley@gmail.com>
2026-04-13 11:13:09 +0200

    Persist VMD translation and rotation on spread

--- shnitsel/vis/vmd/script.tcl
+++ shnitsel/vis/vmd/script.tcl
@@ -20,23 +20,36 @@ proc mtransl {molid x y z} {
 
 proc spread {{sep 0.5} {ncols 5} {vsep_ratio 1} {molids "sentinel"} {ignore "ignore"}} {
     display update off
+
+    set mat [lindex [molinfo top get global_matrix] 0]
+    set old_x [lindex [lindex $mat 0] 3]
+    set old_y [lindex [lindex $mat 1] 3]
+    set old_z [lindex [lindex $mat 2] 3]
+    
     set old_scale [molinfo top get scale_matrix]
+    set old_rotate [molinfo top get rotate_matrix]
+    
     display resetview
     display projection Orthographic
+    
     if {$molids eq "sentinel"} {
         set molids [molinfo list]
     }
     foreach m $molids {
-        puts molid=$m
-        puts sep=$sep
-        puts ncols=$ncols
         set x [expr $sep * ($m % $ncols)]
         set y [expr $vsep_ratio * $sep * ($m / $ncols)]
         mtransl $m $x $y 0
         molinfo $m set scale_matrix $old_scale
+        molinfo $m set rotate_matrix $old_rotate
     }
-    translate by [expr $sep * ($ncols - 1) / 2.0] 0 0
-    # scale to $old_scale
+
+    set mat [lindex [molinfo top get global_matrix] 0]
+    set new_x [lindex [lindex $mat 0] 3]
+    set new_y [lindex [lindex $mat 1] 3]
+    set new_z [lindex [lindex $mat 2] 3]
+
+    translate by [expr $old_x - $new_x] [expr $old_y - $new_y] [expr $old_z - $new_z]
+
     display update on
 }
 
