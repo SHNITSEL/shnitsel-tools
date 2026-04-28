335f20cd95bc8e634c4b6eb89ad83245a2a73a9a
Kevin Höllring <kevin.hoellring@fau.de>
2026-04-28 15:44:30 +0200

    Add debug info output to main shnitsel definition

--- shnitsel/__init__.py
+++ shnitsel/__init__.py
@@ -10,6 +10,7 @@ from shnitsel import (
 # # from shnitsel.core.xrhelpers import open_frames as open_frames
 
 from .io import read, write_shnitsel_file, write_ase_db
+from ._version import *
 
 # # , 'parse', 'open_frames', 'read_trajs', 'read_ase']
 # # __all__ = ['io', 'units']
@@ -26,6 +27,8 @@ __all__ = [
     'read',
     'write_shnitsel_file',
     'write_ase_db',
+    '__version__',
+    'show_debug_info'
 ]
 
 
@@ -39,3 +42,48 @@ def collapse_display():
         display_expand_attrs=False,
         display_expand_data=False,
     )
+
+def show_debug_info():
+    """Helper function to display version and debugging information of shnitsel (and its dependencies).
+    """
+    from packaging.version import parse
+    from importlib.metadata import version
+    from pkgutil import iter_modules
+    import sys
+
+    # Get package version
+    version_info = parse(__version__)
+
+    # Get python environment version
+    python_version = str(sys.version)
+    python_version_info = str(sys.version_info)
+
+    # Signal whether we are on a development version
+    dev_version_string = "yes" if version_info.dev else "no"
+
+    is_additional_version = version_info.is_devrelease or version_info.is_postrelease or version_info.is_prerelease
+    version_suffix_string = ""
+    if is_additional_version:
+        version_suffix_string = f" (full version: {str(version_info)})"
+
+    # Get environment packages
+    packages_str = ""
+    for module_info in iter_modules():
+        if module_info.ispkg:
+            try:
+                packages_str += f"{module_info.name} => {version(module_info.name)}\n"
+            except: 
+                pass
+            # packages_str += f"{module_info.name} => [version missing]\n"
+
+
+    print (f"""
+** SHNITSEL TOOLS v{version_info.public}{version_suffix_string} **
+Commit: {__commit_id__}
+development? {dev_version_string}
+Python-version: {python_version}
+Python-version_info: {python_version_info}
+
+Loaded Packages:
+{packages_str}
+           """)
